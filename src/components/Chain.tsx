9dcc4e68798434a7e4811158c265071f93a07558
Tim Stokman <timstokman@gmail.com>
2021-03-14 21:23:50 +0100

    Typescript conversion

---
+++ src/components/Chain.tsx
@@ -0,0 +1,41 @@
+import withStyles from "@material-ui/core/styles/withStyles";
+import { Link } from "react-router-dom";
+import { IChain } from "../types";
+
+// Material UI
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const styles = {
+  card: {
+    display: "flex",
+  },
+};
+
+interface IChainProps {
+  chain: IChain;
+}
+
+const Chain = (props: IChainProps) => {
+  const { name, description, image} = props.chain;
+  return (
+    <Card>
+      <CardMedia image={image} title="Image" />
+      <CardContent>
+        <Typography
+          variant="h5"
+          component={Link}
+          to={`/chains/${name}`}
+          color="primary"
+        >
+          {name}
+        </Typography>
+        <Typography variant="body1">{description}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default withStyles(styles)(Chain);
