e5601c4f0002b0e344d54c770878b3d11c456011
Pim Sauter <pim@dexterenergy.nl>
2021-03-22 13:18:47 +0100

    Remove API key

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
 import AppIcon from "../images/sfm_logo.png";
-import axios from "axios";
 
 // Material
 import Grid from "@material-ui/core/Grid";
