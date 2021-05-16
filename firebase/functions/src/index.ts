a13524cd7784599b17f17d0ce1db360a5ecfe531
Tim Stokman <timstokman@gmail.com>
2021-05-16 18:42:13 +0200

    E-mail rules

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,4 +1,13 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+const ADMIN_EMAILS = ['pim.sauter@gmail.com', 'timstokman@gmail.com']
+
+export const createUser = functions.https.onRequest(async (request, response) => {
+    let [email, chainId] = [request.body.email, request.body.chainId];
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
