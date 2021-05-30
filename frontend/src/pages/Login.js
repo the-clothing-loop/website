193b02b31493769bfa827412e8e3aa2550625495
Tim Stokman <timstokman@gmail.com>
2021-05-30 04:16:54 +0200

    Login form

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -12,44 +12,53 @@ import theme from "../util/theme";
 // Project resources
 import AppIcon from "../images/sfm_logo.png";
 
+import * as firebase from "firebase/app";
+
 const Login = () => {
   const { register, handleSubmit } = useForm();
   const { t } = useTranslation();
   const classes = makeStyles(theme)();
 
-  const onSubmit = async () => {
-    // TODO: handle this
+  const onSubmit = async (data) => {
+    await firebase.auth()
+                  .sendSignInLinkToEmail(
+                    data.email,
+                    {
+                      handleCodeInApp: false,
+                      url: `https://clothinchain.com/signin?email=${data.email}`
+                    }
+                  );
+    window.localStorage.set('emailForSignIn', data.email);
   };
 
-    return (
-      <Grid container className={classes.form}>
-        <Grid item sm />
-        <Grid item sm>
-          <img
-            src={AppIcon}
-            alt="SFM logo"
-            width="200"
-            className={classes.image}
-          />
-          <Typography variant="h3" className={classes.pageTitle}>
+  return (
+    <Grid container className={classes.form}>
+      <Grid item sm />
+      <Grid item sm>
+        <img
+          src={AppIcon}
+          alt="SFM logo"
+          width="200"
+          className={classes.image}
+        />
+        <Typography variant="h3" className={classes.pageTitle}>
+          {t("login")}
+        </Typography>
+        <form noValidate onSubmit={handleSubmit(onSubmit)}>
+          <TextField {...register('email')} email={true} />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
             {t("login")}
-          </Typography>
-          <form noValidate onSubmit={handleSubmit(onSubmit)}>
-            <TextField fieldName="email" inputRef={register} email={true} />{" "}
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              className={classes.button}
-            >
-              {t("login")}
-            </Button>
-          </form>
-        </Grid>
-        <Grid item sm />
+          </Button>
+        </form>
       </Grid>
-    );
-  };
-
+      <Grid item sm />
+    </Grid>
+  );
+};
 
 export default Login;
