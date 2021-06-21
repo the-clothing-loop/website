f89e4d8b81daed29f3dceb0d21b4e60b904896ff
Giulia Mummolo <mail@giuliamummolo.com>
2021-06-21 14:15:40 +0100

    filters functionality on map

--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -10,6 +10,18 @@ import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Divider from "@material-ui/core/Divider";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import LocalOfferOutlinedIcon from "@material-ui/icons/LocalOfferOutlined";
 
 // Project resources
 import { getChains } from "../util/firebase/chain";
@@ -18,6 +30,12 @@ const accessToken = {
   mapboxApiAccessToken: process.env.REACT_APP_MAPBOX_KEY,
   ipinfoApiAccessToken: process.env.REACT_APP_IPINFO_API_KEY,
 };
+
+const categories = [
+  { gender: "women" },
+  { gender: "men" },
+  { gender: "no gender" },
+];
 
 const Map = () => {
   const history = useHistory();
@@ -32,10 +50,20 @@ const Map = () => {
   const [selectedChain, setSelectedChain] = useState(null);
   const [showPopup, setShowPopup] = useState(false);
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [value, setValue] = useState("");
+
+  const [gender, setGender] = useState([]);
+  const [filteredChains, setFilteredChains] = useState([]);
+
   useEffect(async () => {
     const chainResponse = await getChains();
     setChainData(chainResponse);
-    const userLocationResponse = await getUserLocation(accessToken.ipinfoApiAccessToken);
+    setFilteredChains(chainResponse);
+
+    const userLocationResponse = await getUserLocation(
+      accessToken.ipinfoApiAccessToken
+    );
     if (userLocationResponse.loc) {
       setViewport({
         latitude: Number(userLocationResponse.loc.split(",")[0]),
@@ -50,9 +78,59 @@ const Map = () => {
     }
   }, []);
 
+  const handleChange = (e) => {
+    if (e.target.checked) {
+      setGender([...gender, e.target.value]);
+    } else {
+      setGender(gender.filter((id) => id !== e.target.value));
+    }
+  };
+
+  useEffect(() => {
+    if (gender.length === 0) {
+      setFilteredChains(chainData);
+      return;
+    }
+
+    let filteredArray = chainData.filter((chain) => {
+      if (!chain.categories) {
+        return false; //TODO: do something if a chain has no categories
+      }
+
+      return gender.some((category) => {
+        return chain.categories.gender.includes(category);
+      });
+    });
+
+    setFilteredChains(filteredArray);
+  }, [gender]);
+
   if (!accessToken.mapboxApiAccessToken || !accessToken.ipinfoApiAccessToken) {
     return <div>Access tokens not configured</div>;
   }
+
+  //get search term from input
+  const onChange = (e) => {
+    setSearchTerm(e.target.value);
+
+    chainData.filter((val) => {
+      if (searchTerm == "") {
+      } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return setValue(val);
+      }
+    });
+  };
+
+  //render location on result selection
+  const handleSelect = () => {
+    setViewport({
+      latitude: value.latLon.latitude,
+      longitude: value.latLon.longitude,
+      width: "100vw",
+      height: "100vh",
+      zoom: 8,
+    });
+  };
 
   return (
     <ReactMapGL
@@ -61,25 +139,59 @@ const Map = () => {
       {...viewport}
       onViewportChange={(newView) => setViewport(newView)}
     >
-      {chainData.map((chain) => (
-        <Marker
-          key={chain.id}
-          latitude={chain.latLon.latitude}
-          longitude={chain.latLon.longitude}
-          onClick={(e) => {
-            e.preventDefault();
-            setSelectedChain(chain);
-            setShowPopup(true);
-          }}
-        >
-          {" "}
-          <img
-            id="marker"
-            src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png"
-            alt="Map Marker"
+      <div className={"filter-wrapper"}>
+        <Paper component="form" className={styles.root2}>
+          <InputBase
+            className={styles.input}
+            placeholder="Search For Chain"
+            inputProps={{ "aria-label": "search for chain" }}
+            onChange={onChange}
           />
-        </Marker>
-      ))}
+          <IconButton
+            type="submit"
+            className={styles.iconButton}
+            aria-label="search"
+          >
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        {value ? <Button onClick={handleSelect}>{value.name}</Button> : null}
+
+        <FormControl>
+          <FormGroup>
+            {categories.map((cat) => (
+              <FormControlLabel
+                control={<Checkbox onChange={handleChange} />}
+                label={cat.gender}
+                value={cat.gender}
+                key={cat.gender}
+              />
+            ))}
+          </FormGroup>
+        </FormControl>
+      </div>
+
+      {filteredChains.map((chain) =>
+        chain.published ? (
+          <Marker
+            key={chain.id}
+            latitude={chain.latLon.latitude}
+            longitude={chain.latLon.longitude}
+            onClick={(e) => {
+              e.preventDefault();
+              setSelectedChain(chain);
+              setShowPopup(true);
+            }}
+          >
+            {" "}
+            <img
+              id="marker"
+              src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png"
+              alt="Map Marker"
+            />
+          </Marker>
+        ) : null
+      )}
 
       {selectedChain && showPopup ? (
         <Popup
@@ -93,7 +205,6 @@ const Map = () => {
             <CardContent>
               <Typography
                 className={styles.title}
-                color="secondary.dark"
                 component="p"
                 variant="h4"
                 gutterBottom
@@ -105,12 +216,31 @@ const Map = () => {
                 component="p"
                 className={"chain-address"}
               >
+                <LocationOnOutlinedIcon />
                 {selectedChain.address}
               </Typography>
               <Typography variant="body2" component="p">
                 {selectedChain.description}
               </Typography>
+              <Divider variant="middle" />
+              <div className={"chain-categories"}>
+                {selectedChain.categories
+                  ? selectedChain.categories.gender.map((category) => {
+                      return (
+                        <Typography
+                          variant="body2"
+                          component="p"
+                          display="inline"
+                        >
+                          <LocalOfferOutlinedIcon display="inline" />
+                          {category}
+                        </Typography>
+                      );
+                    })
+                  : null}
+              </div>
             </CardContent>
+
             <CardActions>
               <Button
                 variant="contained"
