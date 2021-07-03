4e494409592c5ee5c5179e69ca82d5bee86c94ae
Giulia Mummolo <mail@giuliamummolo.com>
2021-07-03 17:12:23 +0100

    Customizing navbar based on user role

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
+import { useState, useEffect } from "react";
 
 // Material UI
 import { AppBar, Toolbar, Button } from "@material-ui/core";
@@ -11,7 +12,10 @@ import { AuthContext } from "../components/AuthProvider";
 
 const Navbar = () => {
   const { t } = useTranslation();
-  const { user } = useContext(AuthContext);
+  // const { user } = useContext(AuthContext);
+  const { user, userData } = useContext(AuthContext);
+
+  console.log(userData);
 
   return (
     // Use sticky position to make content start below the Navbar, instead of being covered by it.
@@ -27,14 +31,31 @@ const Navbar = () => {
         <Button color="inherit" component={Link} to="/chains/new-signup">
           {t("startNewChain")}
         </Button>
-        { user ?
+        {user ? (
           <Button color="inherit" component={Link} to="/users/logout">
             {t("logout")}
-          </Button> :
+          </Button>
+        ) : (
           <Button color="inherit" component={Link} to="/users/login">
             {t("login")}
-          </Button> 
-        }
+          </Button>
+        )}
+
+        {userData != null && userData.role === "admin" ? (
+          <Button color="inherit" component={Link} to="/chains">
+            {t("admin")}
+          </Button>
+        ) : null}
+
+        {userData != null && userData.role === "chainAdmin" ? (
+          <Button
+            color="inherit"
+            component={Link}
+            to={`/chains/members/${userData.chainId}`}
+          >
+            {t("admin")}
+          </Button>
+        ) : null}
       </Toolbar>
       <LanguageSwitcher />
     </AppBar>
