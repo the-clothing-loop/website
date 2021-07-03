4cf30d5bb8e91114f392dbd96d1d127fc4c25e3b
Giulia Mummolo <52887737+giuliamml@users.noreply.github.com>
2021-07-03 18:14:27 +0100

    Customizing navbar based on user role (#70)
    * Customizing navbar based on user role
    
    * Customizing navbar based on user role
    
    * removed logs
    
    * removed code
    
    * refactored code with null coalescion operator

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -9,6 +9,7 @@ const db = admin.firestore();
 const ADMIN_EMAILS = [
   "pim.sauter@gmail.com",
   "timstokman@gmail.com",
+  "mail@giuliamummolo.com",
 ];
 
 const BASE_DOMAIN = "http://localhost:3000";
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,7 +11,7 @@ import { AuthContext } from "../components/AuthProvider";
 
 const Navbar = () => {
   const { t } = useTranslation();
-  const { user } = useContext(AuthContext);
+  const { user, userData } = useContext(AuthContext);
 
   return (
     // Use sticky position to make content start below the Navbar, instead of being covered by it.
@@ -27,14 +27,31 @@ const Navbar = () => {
         <Button color="inherit" component={Link} to="/chains/new-signup">
           {t("startNewChain")}
         </Button>
-        { user ?
+        {user ? (
           <Button color="inherit" component={Link} to="/users/logout">
             {t("logout")}
-          </Button> :
+          </Button>
+        ) : (
           <Button color="inherit" component={Link} to="/users/login">
             {t("login")}
-          </Button> 
-        }
+          </Button>
+        )}
+
+        {userData?.role === "admin" ? (
+          <Button color="inherit" component={Link} to="/chains">
+            {t("admin")}
+          </Button>
+        ) : null}
+
+        {userData?.role === "chainAdmin" ? (
+          <Button
+            color="inherit"
+            component={Link}
+            to={`/chains/members/${userData.chainId}`}
+          >
+            {t("admin")}
+          </Button>
+        ) : null}
       </Toolbar>
       <LanguageSwitcher />
     </AppBar>
