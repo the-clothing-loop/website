b0a39248c6f3d340f4d4aa7829e3b7567b4405d4
Giulia Mummolo <mail@giuliamummolo.com>
2021-07-06 17:09:44 +0100

    publish chain functionality

--- frontend/src/pages/ChainMemberList.tsx
+++ frontend/src/pages/ChainMemberList.tsx
@@ -13,9 +13,12 @@ import EditIcon from "@material-ui/icons/Edit";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import Switch from "@material-ui/core/Switch";
+import { Alert } from "@material-ui/lab";
+import LocalOfferOutlinedIcon from "@material-ui/icons/LocalOfferOutlined";
 
 // Project resources
-import { getChain } from "../util/firebase/chain";
+import { getChain, updateChain } from "../util/firebase/chain";
 import { getUsersForChain } from "../util/firebase/user";
 import { IChain, IUser } from "../types";
 
@@ -27,9 +30,29 @@ const ChainMemberList = () => {
   const { t } = useTranslation();
   const location = useLocation<any>();
   const { chainId } = useParams<TParams>();
+
   const [chain, setChain] = useState<IChain>();
   const [users, setUsers] = useState<IUser[]>();
+  const [publishedValue, setPublishedValue] = useState({ published: true });
+  const [error, setError] = useState("");
 
+  const handleChange = (e: { target: { checked: boolean; name: any } }) => {
+    setPublishedValue({ ...publishedValue, [e.target.name]: e.target.checked });
+
+    const updatedChainData = {
+      [e.target.name]: e.target.checked,
+    };
+
+    console.log(`updating chain data: ${JSON.stringify(updatedChainData)}`);
+    try {
+      updateChain(chainId, updatedChainData);
+    } catch (e) {
+      console.error(`Error updating chain: ${JSON.stringify(e)}`);
+      setError(e.message);
+    }
+  };
+
+  console.log(location);
   useEffect(() => {
     (async () => {
       try {
@@ -37,74 +60,123 @@ const ChainMemberList = () => {
         setChain(chainData);
         const users = await getUsersForChain(chainId);
         setUsers(users);
+        setPublishedValue({ published: chainData.published });
       } catch (error) {
         console.error(error);
       }
     })();
   }, []);
 
-  return !chain || !users ? null : <>
-    <Helmet>
-      <title>Clothing-chain | Chain members</title>
-      <meta name="description" content="Chain members" />
-    </Helmet>
-    <div className="chain-member-list">
-      <Card>
-        <CardContent>
-          <Typography variant="h3" component="h2">
-            {chain.name}
-          </Typography>
-          <Typography
-            variant="body2"
-            component="p"
-            style={{ display: "inline" }}
-          >
-            {chain.description}
-          </Typography>{" "}
-          {" | "}
-          <Typography
-            variant="body2"
-            component="p"
-            style={{ display: "inline" }}
-          >
-            {chain.address}
-          </Typography>
-          <Link to={`/chains/edit/${chainId}`}>
-            <EditIcon />
-          </Link>
-          {location.state ? (
-            <p className="success">{location.state.message}</p>
-          ) : null}
-        </CardContent>
-      </Card>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>{t("name")}</TableCell>
-            <TableCell>{t("address")}</TableCell>
-            <TableCell>{t("phoneNumber")}</TableCell>
-            <TableCell>{t("email")}</TableCell>
-            <TableCell />
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((u: IUser) => (
-            <TableRow key={u.uid}>
-              <TableCell>{u.name}</TableCell>
-              <TableCell>{u.address}</TableCell>
-              <TableCell>{u.phoneNumber}</TableCell>
-              <TableCell>{u.email}</TableCell>
-              <TableCell align="right">
-                <Link to={`/users/edit/${u.uid}`}>
-                  <EditIcon />
-                </Link>
-              </TableCell>
+  return !chain || !users ? null : (
+    <>
+      <Helmet>
+        <title>Clothing-chain | Chain members</title>
+        <meta name="description" content="Chain members" />
+      </Helmet>
+      <div className="chain-member-list">
+        <Card>
+          <CardContent>
+            {location.state ? (
+              <p className="success">{location.state.message}</p>
+            ) : null}
+            <Typography variant="h3" component="h2">
+              {chain.name}
+            </Typography>
+            <Link to={`/chains/edit/${chainId}`} className="chain-edit-btn">
+              <EditIcon />
+            </Link>
+
+            <Typography
+              variant="body1"
+              component="p"
+              style={{ display: "inline" }}
+            >
+              {chain.address}
+            </Typography>
+
+            <Typography
+              variant="body1"
+              component="p"
+              style={{ display: "inline" }}
+              color="textSecondary"
+            >
+              {chain.description}
+            </Typography>
+
+            <div className={"chain-categories-admin"}>
+              {chain.categories
+                ? chain.categories.gender.map((gender, i) => {
+                    return (
+                      <Typography
+                        variant="body1"
+                        component="p"
+                        display="inline"
+                        key={i}
+                      >
+                        <LocalOfferOutlinedIcon />
+                        {gender}
+                      </Typography>
+                    );
+                  })
+                : null}
+            </div>
+            <div className="switch-wrapper">
+              <Switch
+                checked={publishedValue.published}
+                onChange={handleChange}
+                name="published"
+                inputProps={{ "aria-label": "secondary checkbox" }}
+              />
+              {publishedValue.published ? (
+                <Typography
+                  variant="body1"
+                  component="p"
+                  style={{ color: "green" }}
+                >
+                  {"published"}
+                </Typography>
+              ) : (
+                <Typography
+                  variant="body1"
+                  component="p"
+                  style={{ color: "red" }}
+                >
+                  {"not published"}
+                </Typography>
+              )}
+              {error ? <Alert severity="error">{error}</Alert> : null}
+            </div>
+          </CardContent>
+        </Card>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>{t("name")}</TableCell>
+              <TableCell>{t("address")}</TableCell>
+              <TableCell>{t("phoneNumber")}</TableCell>
+              <TableCell>{t("email")}</TableCell>
+              <TableCell />
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
-  </>;
+          </TableHead>
+          <TableBody>
+            {users.map((u: IUser) => (
+              <TableRow key={u.uid}>
+                <TableCell>{u.name}</TableCell>
+                <TableCell>{u.address}</TableCell>
+                <TableCell>{u.phoneNumber}</TableCell>
+                <TableCell>{u.email}</TableCell>
+                <TableCell align="right">
+                  <Link to={`/users/edit/${u.uid}`}>
+                    <EditIcon />
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
 };
 
 export default ChainMemberList;
