77918294709879d066676618588922c2fe27daba
Giulia Mummolo <mail@giuliamummolo.com>
2021-07-08 12:20:52 +0100

    edit chain categories and coordinates

--- frontend/src/pages/ChainEdit.js
+++ frontend/src/pages/ChainEdit.js
@@ -6,7 +6,6 @@ import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
 // Material UI
-import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core";
@@ -15,8 +14,7 @@ import theme from "../util/theme";
 // Project resources
 import { getChain, updateChain } from "../util/firebase/chain";
 import GeocoderSelector from "../components/GeocoderSelector";
-import { boolean } from "yup/lib/locale";
-import { TextForm } from "../components/FormFields";
+import { TextForm, CheckboxField } from "../components/FormFields";
 import ThreeColumnLayout from "../components/ThreeColumnLayout";
 
 const ChainEdit = () => {
@@ -28,23 +26,49 @@ const ChainEdit = () => {
   const [chain, setChain] = useState();
   const classes = makeStyles(theme)();
   const [address, setAddress] = useState();
+  const [coordinates, setCoordinates] = useState();
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState("");
+  const [categoriesState, setCategoriesState] = useState({});
 
   const validate = Yup.object({
     name: Yup.string().min(2, "Must be more than 2 characters"),
     description: Yup.string().min(2, "Must be more than 2 characters"),
-    published: Yup.boolean(),
   });
 
-  const onClick = () => {
-    history.push(`/chains/members/${chainId}`);
+  const handleChange = (e) => {
+    if (e.target.checked) {
+      setCategoriesState({
+        ...categoriesState,
+        [e.target.name]: true,
+      });
+    } else {
+      setCategoriesState({
+        ...categoriesState,
+        [e.target.name]: false,
+      });
+    }
+  };
+
+  //refactor db data from array to obj
+  const mapCat = (array) => {
+    return {
+      men: array.includes("men"),
+      woman: array.includes("woman"),
+      mix: array.includes("mix"),
+    };
   };
 
   useEffect(async () => {
     const chain = await getChain(chainId);
     setChain(chain);
     setAddress(chain.address);
+    setCoordinates({
+      latitude: chain.latitude,
+      longitude: chain.longitude,
+    });
+
+    setCategoriesState(mapCat(chain.categories.gender));
   }, []);
 
   return !chain ? null : (
@@ -63,13 +87,20 @@ const ChainEdit = () => {
           const newChainData = {
             ...values,
             address: address,
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            categories: {
+              gender: Object.keys(categoriesState).filter((e) => {
+                return categoriesState[e] === true;
+              }),
+            },
           };
 
           console.log(
             `updating chain information: ${JSON.stringify(newChainData)}`
           );
           try {
-            updateChain(chainId, newChainData);
+            await updateChain(chainId, newChainData);
             setSubmitted(true);
             history.push({
               pathname: `/chains/members/${chainId}`,
@@ -110,8 +141,33 @@ const ChainEdit = () => {
                 }
               />
 
+              <CheckboxField
+                name="woman"
+                label="woman"
+                onChange={handleChange}
+                checked={categoriesState.woman ? true : false}
+              />
+              <CheckboxField
+                name="men"
+                label="men"
+                onChange={handleChange}
+                checked={categoriesState.men ? true : false}
+              />
+              <CheckboxField
+                name="mix"
+                label="mix"
+                onChange={handleChange}
+                checked={categoriesState.mix ? true : false}
+              />
+
               <GeocoderSelector
-                onResult={(e) => setAddress(e.result.place_name)}
+                onResult={(e) => {
+                  setAddress(e.result.place_name);
+                  setCoordinates({
+                    longitude: e.result.geometry.coordinates[0],
+                    latitude: e.result.geometry.coordinates[1],
+                  });
+                }}
               />
 
               <Button
@@ -123,7 +179,7 @@ const ChainEdit = () => {
                 {t("submit")}
               </Button>
               <Button
-                onClick={onClick}
+                onClick={() => history.push(`/chains/members/${chainId}`)}
                 variant="contained"
                 color="primary"
                 className={classes.button}
