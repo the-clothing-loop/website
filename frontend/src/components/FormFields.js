034b1e08d10ee4b3f030bd20fe64b1884ea16877
kfless12 <46584348+kfless12@users.noreply.github.com>
2021-09-10 15:53:48 -0400

    fixed more linting errors

--- frontend/src/components/FormFields.js
+++ frontend/src/components/FormFields.js
@@ -1,84 +1,84 @@
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import theme from "../util/theme";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 import MuiPhoneInput from "material-ui-phone-number";
 import Checkbox from "@material-ui/core/Checkbox";
-import { useField } from "formik";
+import {useField} from "formik";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-const TextFormField = ({ name, inputRef, email }) => {
-	const { t } = useTranslation();
-	const classes = makeStyles(theme)();
+const TextFormField = ({name, inputRef, email}) => {
+  const {t} = useTranslation();
+  const classes = makeStyles(theme)();
 
-	return (
-		<TextField
-			id={name}
-			name={name}
-			type={email ? "email" : "text"}
-			label={t(name)}
-			className={classes.textField}
-			inputRef={inputRef}
-			required={true}
-			fullWidth
-		></TextField>
-	);
+  return (
+    <TextField
+      id={name}
+      name={name}
+      type={email ? "email" : "text"}
+      label={t(name)}
+      className={classes.textField}
+      inputRef={inputRef}
+      required={true}
+      fullWidth
+    ></TextField>
+  );
 };
 
-const PhoneFormField = ({ label, ...props }) => {
-	const { t } = useTranslation();
+const PhoneFormField = ({label, ...props}) => {
+  const {t} = useTranslation();
 
-	const [field, meta] = useField(props);
-	return (
-		<div>
-			<MuiPhoneInput
-				defaultCountry="nl"
-				regions={"europe"}
-				placeholder="Enter phone number"
-				fullWidth
-				label="phone"
-				required={true}
-				htmlFor={field.name}
-				{...field}
-				{...props}
-			></MuiPhoneInput>
-		</div>
-	);
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <MuiPhoneInput
+        defaultCountry="nl"
+        regions={"europe"}
+        placeholder="Enter phone number"
+        fullWidth
+        label="phone"
+        required={true}
+        htmlFor={field.name}
+        {...field}
+        {...props}
+      ></MuiPhoneInput>
+    </div>
+  );
 };
 
-const TextForm = ({ label, ...props }) => {
-	const [field, meta] = useField(props);
-	const { t } = useTranslation();
+const TextForm = ({label, ...props}) => {
+  const [field, meta] = useField(props);
+  const {t} = useTranslation();
 
-	return (
-		<div>
-			<TextField
-				{...field}
-				{...props}
-				autoComplete="off"
-				label={t(label)}
-				fullWidth
-			/>
-		</div>
-	);
+  return (
+    <div>
+      <TextField
+        {...field}
+        {...props}
+        autoComplete="off"
+        label={t(label)}
+        fullWidth
+      />
+    </div>
+  );
 };
 
-const CheckboxField = ({ label, ...props }) => {
-	const [field, meta] = useField(props);
-	const { t } = useTranslation();
-	console.log(props);
+const CheckboxField = ({label, ...props}) => {
+  const [field, meta] = useField(props);
+  const {t} = useTranslation();
+  console.log(props);
 
-	return (
-		<FormGroup row>
-			<FormControlLabel
-				control={<Checkbox name={field.name} required={props.req || false} />}
-				{...field}
-				{...props}
-				label={label}
-			/>
-		</FormGroup>
-	);
+  return (
+    <FormGroup row>
+      <FormControlLabel
+        control={<Checkbox name={field.name} required={props.req || false} />}
+        {...field}
+        {...props}
+        label={label}
+      />
+    </FormGroup>
+  );
 };
 
-export { TextFormField, PhoneFormField, TextForm, CheckboxField };
+export {TextFormField, PhoneFormField, TextForm, CheckboxField};
