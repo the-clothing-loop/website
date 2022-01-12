2f6c16cf86ae4f3f40530ed8b9add24f30e31421
Giulia Mummolo <52887737+giuliamml@users.noreply.github.com>
2022-01-12 17:26:20 +0000

    Header/footer and internationalization fix (#138)

--- frontend/src/components/LanguageSwitcher.tsx
+++ frontend/src/components/LanguageSwitcher.tsx
@@ -11,11 +11,10 @@ import { makeStyles } from "@material-ui/core";
 import theme from "../util/theme";
 
 const LanguageSwitcher = () => {
-  const languages = ["english", "dutch"];
+  const languages = ["en", "nl"];
   const classes = makeStyles(theme as any)();
 
-
-  const [selected, setSelected] = useState("english");
+  const [selected, setSelected] = useState("en");
 
   const handleChange = (e: any) => {
     let language = e.target.value;
@@ -37,7 +36,7 @@ const LanguageSwitcher = () => {
             {languages.map((el, i) => {
               return (
                 <MenuItem className={classes.menuItem} key={i} value={el}>
-                  {el}
+                  {el === "en" ? "English 🇬🇧" : "Dutch 🇳🇱"}
                 </MenuItem>
               );
             })}
