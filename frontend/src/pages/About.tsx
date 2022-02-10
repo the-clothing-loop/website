e90334fdac551e5c7b1f0189126f6fd18287c2ad
Giulia Mummolo <mail@giuliamummolo.com>
2022-02-10 12:10:08 +0000

    fixed iframe bug

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -16,12 +16,26 @@ const About = () => {
       </Typography>
       <div className={classes.legalPagesContentWrapper}>
         <div className="iframe-wrapper">
-          <iframe
-            src="https://player.vimeo.com/video/673700502?h=90c8532936"
-            width="640"
-            height="360"
-            allow="autoplay; fullscreen; picture-in-picture"
-          ></iframe>
+          <div
+            style={{
+              padding: "56.25% 0 0 0",
+              position: "relative",
+              left: "50%",
+            }}
+          >
+            <iframe
+              src="https://player.vimeo.com/video/673700502?h=90c8532936&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+              allow="autoplay; fullscreen; picture-in-picture"
+              style={{
+                position: "absolute",
+                top: "0",
+                left: "0",
+                width: "100%",
+                height: "100%",
+              }}
+            ></iframe>
+          </div>
+          <script src="https://player.vimeo.com/api/player.js"></script>
         </div>
         <Typography component="p">
           The Clothing Loop is an initiative that allows people to easily swap
