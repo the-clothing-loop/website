547b39a012ed8591de748ba64c727d8d65c6ab2d
tberm <tomas.bermejo@gmail.com>
2022-02-20 16:16:20 +0000

    Add close button to Dutch loops dialog

--- frontend/src/pages/FindChain.tsx
+++ frontend/src/pages/FindChain.tsx
@@ -15,7 +15,7 @@ import * as GeoJSONTypes from "geojson";
 import mapboxgl from "mapbox-gl";
 
 // Material UI
-import { Button, Dialog } from "@material-ui/core";
+import { Button, Dialog, IconButton } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -24,7 +24,7 @@ import { makeStyles } from "@material-ui/core";
 import GpsFixedIcon from "@mui/icons-material/GpsFixed";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
-import CircleIcon from "@mui/icons-material/Circle";
+import CloseIcon from "@mui/icons-material/Close";
 
 // Project resources
 import { ChainsContext } from "../components/ChainsProvider";
@@ -302,6 +302,13 @@ const FindChain = () => {
         onClose={() => setShowDutchLoopsDialog(false)}
       > 
         <DutchLoopsCard />
+        <IconButton
+          aria-label="close"
+          onClick={() => setShowDutchLoopsDialog(false)}
+          className={classes.dialogCloseButton}
+        >
+          <CloseIcon />
+        </IconButton>
       </Dialog>
 
       <ReactMapGL
