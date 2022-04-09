84b6557b19419bed37e107664edbf0edb70effe0
tberm <tomas.bermejo@gmail.com>
2022-04-09 17:52:23 +0100

    Auto-fill search form and perform search using query string

--- frontend/src/components/FindChain/index.tsx
+++ frontend/src/components/FindChain/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import * as Yup from "yup";
 
 import { SearchBar } from "./SearchBar";
 import { ChainNotFound } from "./ChainNotFound";
 
 import { IChain } from "../../types";
+import { ChainsContext } from "../ChainsProvider";
 import { defaultTruePredicate, ChainPredicate } from "../../pages/FindChain";
 
 const hasCommonElements = (arr1: string[], arr2: string[]) =>
@@ -12,16 +14,20 @@ const hasCommonElements = (arr1: string[], arr2: string[]) =>
 interface IProps {
   setFilterChainPredicate: React.Dispatch<React.SetStateAction<ChainPredicate>>;
   handleFindChainCallback: (findChainPredicate: ChainPredicate) => boolean;
+  initialValues?: any;
 }
 
 export const FindChainSearchBarContainer = ({
   setFilterChainPredicate,
   handleFindChainCallback,
+  initialValues,
 }: IProps) => {
   const [searchTerm, setSearchTerm] = React.useState<string>("");
   const [selectedSizes, setSelectedSizes] = React.useState<string[]>([]);
   const [selectedGenders, setSelectedGenders] = React.useState<string[]>([]);
   const [isChainNotFound, setIsChainNotFound] = React.useState<boolean>(false);
+  const [formAutoFilled, setFormAutoFilled] = React.useState<boolean>(false);
+  const chains = useContext(ChainsContext);
 
   const handleSearchTermChange = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -70,6 +76,34 @@ export const FindChainSearchBarContainer = ({
     setIsChainNotFound(false);
     setFilterChainPredicate(() => defaultTruePredicate);
   };
+
+  useEffect(() => {
+    // chains may not have loaded yet
+    if (!chains.length || !Object.keys(initialValues).length) {
+      return;
+    }
+    const schema = Yup.object({
+      searchTerm: Yup.string().default(""),
+      sizes: Yup.array().of(Yup.string()).default([]),
+      genders: Yup.array().of(Yup.string()).default([]),
+    });
+
+    try {
+      let validated = schema.validateSync(initialValues);
+      setSearchTerm(validated.searchTerm);
+      setSelectedSizes(validated.sizes);
+      setSelectedGenders(validated.genders);
+      setFormAutoFilled(true);
+    } catch (error) {
+      console.error("Invalid query string parameters");
+      console.error(error);
+      return;
+    }
+  }, [chains]);
+
+  useEffect(() => {
+    handleSearch();
+  }, [formAutoFilled]);
 
   return (
     <>
