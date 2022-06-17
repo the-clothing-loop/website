6b97318eb261c1f40e3f155e8b24e73292f868d8
Lucian I. Last <li@last.nl>
2022-06-17 10:48:04 +0200

    Add important root for higher specificity

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   prefix: "tw-",
+  important: "#root",
   theme: {
     screens: {
       xs: "481px",
