6377bb4605d671c43ae103223d9c0db9c60f52c3
Lucian I. Last <li@last.nl>
2022-06-19 15:00:31 +0200

    Move back to react-scripts & Generate tailwind using cli

--- frontend/craco.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  style: {
-    postcss: {
-      plugins: [require("tailwindcss")("./tailwind.config.js")],
-    },
-  },
-};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./i18n";
-import "./tailwind.css";
+import "./tailwind.generated.css";
 
 ReactDOM.render(
   <React.StrictMode>
