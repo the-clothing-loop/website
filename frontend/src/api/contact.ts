6ae20cb81cb1cf0b63808b6369b65861b7734ce4
Lucian I. Last <li@last.nl>
2022-08-26 17:34:06 +0200

    migrate newsletter

--- frontend/src/api/contact.ts
+++ frontend/src/api/contact.ts
@@ -1,13 +1,13 @@
 import axios from "./axios";
 
-export function postNewsletter(email: string, subscribe: boolean) {
+export function contactNewsletterSet(email: string, subscribe: boolean) {
   return axios.post("/v1/contact/newsletter", {
     email,
     subscribe,
   });
 }
 
-export function postMail(name: string, email: string, message: string) {
+export function contactMailSend(name: string, email: string, message: string) {
   return axios.post("/v1/contact/email", {
     name,
     email,
