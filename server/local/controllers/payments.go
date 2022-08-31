b67279e76300030a5147f50c789da93e844b7030
Lucian I. Last <li@last.nl>
2022-08-31 15:48:01 +0200

    Working webhook route

--- server/local/controllers/payments.go
+++ server/local/controllers/payments.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -12,7 +13,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/stripe/stripe-go/v73"
 	stripe_session "github.com/stripe/stripe-go/v73/checkout/session"
+	stripe_customer "github.com/stripe/stripe-go/v73/customer"
+	stripe_subscription "github.com/stripe/stripe-go/v73/subscription"
 	stripe_webhook "github.com/stripe/stripe-go/v73/webhook"
+	"gorm.io/gorm"
 )
 
 // Rewrite of https://github.com/CollActionteam/clothing-loop/blob/e5d09d38d72bb42f531d0dc0ec7a5b18459bcbcd/firebase/functions/src/payments.ts#L18
@@ -30,11 +34,8 @@ func PaymentsInitiate(c *gin.Context) {
 
 	db := getDB(c)
 
-	paymentMethodTypeIDEAL := "ideal"          // stripe.PaymentMethodTypeIDEAL
-	paymentMethodTypeCard := "card"            // stripe.PaymentMethodTypeCard
-	paymentMethodTypeSEPADebit := "sepa_debit" // stripe.PaymentMethodTypeSEPADebit
-	checkoutSessionModePayment := "payment"    // stripe.CheckoutSessionModePayment
-	checkoutSessionModeSetup := "setup"        // stripe.CheckoutSessionModeSetup
+	checkoutSessionModePayment := string(stripe.CheckoutSessionModePayment)
+	checkoutSessionModeSetup := string(stripe.CheckoutSessionModeSetup)
 	currency := "eur"
 	quantity := int64(1)
 	name := "Donation"
@@ -46,10 +47,16 @@ func PaymentsInitiate(c *gin.Context) {
 	checkout := new(stripe.CheckoutSessionParams)
 
 	if body.IsRecurring {
-		checkout.PaymentMethodTypes = []*string{&paymentMethodTypeSEPADebit, &paymentMethodTypeCard}
+		checkout.PaymentMethodTypes = stripe.StringSlice([]string{
+			string(stripe.PaymentMethodTypeSEPADebit),
+			string(stripe.PaymentMethodTypeCard),
+		})
 		checkout.Mode = &checkoutSessionModeSetup
 	} else {
-		checkout.PaymentMethodTypes = []*string{&paymentMethodTypeIDEAL, &paymentMethodTypeCard}
+		checkout.PaymentMethodTypes = stripe.StringSlice([]string{
+			string(stripe.PaymentMethodTypeIDEAL),
+			string(stripe.PaymentMethodTypeCard),
+		})
 		checkout.Mode = &checkoutSessionModePayment
 		checkout.LineItems = []*stripe.CheckoutSessionLineItemParams{
 			{
@@ -81,10 +88,11 @@ func PaymentsInitiate(c *gin.Context) {
 	}
 
 	if err := db.Create(&models.Payment{
-		SessionID:   session.ID,
-		Amount:      float32(session.AmountTotal) / 100,
-		Email:       customerEmail,
-		IsRecurring: body.IsRecurring,
+		Amount:          float32(session.AmountTotal) / 100,
+		Email:           customerEmail,
+		IsRecurring:     body.IsRecurring,
+		SessionStripeID: session.ID,
+		Status:          string(session.Status),
 	}).Error; err != nil {
 		log.Print(err)
 		boom.Internal(c.Writer, "unable to add payment to database")
@@ -112,6 +120,8 @@ func PaymentsWebhook(c *gin.Context) {
 		return
 	}
 
+	log.Printf("event.Type: %+v", event.Type)
+
 	switch event.Type {
 	case "checkout.session.completed":
 		paymentsWebhookCheckoutSessionCompleted(c, event)
@@ -121,8 +131,93 @@ func PaymentsWebhook(c *gin.Context) {
 }
 
 func paymentsWebhookCheckoutSessionCompleted(c *gin.Context, event stripe.Event) {
-	session := event.Data.Object
+	db := getDB(c)
 
-	log.Printf("paymentsWebhookCheckoutSessionCompleted: %+v", session)
-	log.Printf("metadata %+v", session["metadata"])
+	session := new(stripe.CheckoutSession)
+	err := json.Unmarshal(event.Data.Raw, session)
+	if err != nil {
+		log.Print(err)
+		boom.Internal(c.Writer)
+		return
+	}
+
+	switch session.Mode {
+	case stripe.CheckoutSessionModeSetup:
+		paymentsWebhookCheckoutSessionCompletedModeSetup(c, db, session)
+	case stripe.CheckoutSessionModePayment:
+		paymentsWebhookCheckoutSessionCompletedModePayment(c, db, session)
+	default:
+		c.JSON(200, gin.H{"received": true})
+	}
+}
+
+func paymentsWebhookCheckoutSessionCompletedModeSetup(c *gin.Context, db *gorm.DB, session *stripe.CheckoutSession) {
+	priceID := session.Metadata["price_id"]
+	intent := session.SetupIntent
+	billingDetails := session.PaymentIntent.PaymentMethod.BillingDetails
+	paymentMethodType := (*string)(&intent.PaymentMethod.Type)
+
+	customer, err := stripe_customer.New(&stripe.CustomerParams{
+		Email: &billingDetails.Email,
+		Name:  &billingDetails.Name,
+		Address: &stripe.AddressParams{
+			City:       &billingDetails.Address.City,
+			Country:    &billingDetails.Address.Country,
+			Line1:      &billingDetails.Address.Line1,
+			Line2:      &billingDetails.Address.Line2,
+			PostalCode: &billingDetails.Address.PostalCode,
+			State:      &billingDetails.Address.State,
+		},
+		PaymentMethod: paymentMethodType,
+		InvoiceSettings: &stripe.CustomerInvoiceSettingsParams{
+			DefaultPaymentMethod: paymentMethodType,
+		},
+	})
+	if err != nil {
+		log.Print(err)
+		boom.Internal(c.Writer, "Unable to create customer")
+		return
+	}
+
+	_, err = stripe_subscription.New(&stripe.SubscriptionParams{
+		Customer: &customer.ID,
+		Items: []*stripe.SubscriptionItemsParams{
+			{Price: &priceID},
+		},
+	})
+	if err != nil {
+		log.Print(err)
+		boom.Internal(c.Writer, "Unable to create subscription")
+		return
+	}
+
+	err = db.Model(&models.Payment{}).Where("session_stripe_id = ?", session.ID).Updates(&models.Payment{
+		Status:                string(session.Status),
+		CustomerStripeID:      customer.ID,
+		PaymentIntentStripeID: session.PaymentIntent.ID,
+	}).Error
+	if err != nil {
+		log.Print(err)
+		boom.Internal(c.Writer, "Unable to update payment in database")
+		return
+	}
+
+	c.JSON(200, gin.H{"received": true})
+}
+
+func paymentsWebhookCheckoutSessionCompletedModePayment(c *gin.Context, db *gorm.DB, session *stripe.CheckoutSession) {
+	// ! Warning session.Customer is nil here
+	err := db.Model(&models.Payment{}).Where("session_stripe_id = ?", session.ID).UpdateColumns(&models.Payment{
+		Status:                string(session.Status),
+		CustomerStripeID:      "",
+		PaymentIntentStripeID: session.PaymentIntent.ID,
+		Email:                 session.CustomerEmail,
+	}).Error
+	if err != nil {
+		log.Print(err)
+		boom.Internal(c.Writer, "Unable to update payment in database")
+		return
+	}
+
+	c.JSON(200, gin.H{"received": true})
 }
--- server/local/models/payment.go
+++ server/local/models/payment.go
@@ -1,13 +1,27 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/stripe/stripe-go/v73"
+)
+
+const (
+	PaymentStatusEnumComplete = stripe.CheckoutSessionStatusComplete
+	PaymentStatusEnumOpen     = stripe.CheckoutSessionStatusOpen
+	PaymentStatusEnumExpired  = stripe.CheckoutSessionStatusExpired
+)
 
 type Payment struct {
 	ID uint
 	// Euro cents
-	Amount      float32
-	Email       string
-	IsRecurring bool
-	SessionID   string
-	CreatedAt   time.Time
+	Amount                float32
+	Email                 string
+	IsRecurring           bool
+	SessionStripeID       string `gorm:"uniqueIndex"`
+	CustomerStripeID      string
+	PaymentIntentStripeID string
+	Status                string
+	CreatedAt             time.Time
+	UpdatedAt             time.Time
 }
