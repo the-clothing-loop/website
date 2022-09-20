38c56f6e80f6e1c5f44fbcf18c8a86e8c58f4dff
Lucian I. Last <li@last.nl>
2022-09-20 10:10:49 +0200

    Remove TestConfigWithProductionEnv

--- server/local/app/config_test.go
+++ server/local/app/config_test.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,11 +10,7 @@ func TestConfigInit(t *testing.T) {
 	ConfigTestInit("../..")
 	assert.NotEqual(t, 0, Config.DB_PORT, "db_port not set")
 }
-func TestConfigWithProductionEnv(t *testing.T) {
-	os.Setenv("SERVER_ENV", EnvEnumProduction)
-	ConfigInit("../..")
-	assert.Equal(t, EnvEnumProduction, Config.ENV, "env is not set to production")
-}
+
 func TestConfigWithTestingEnv(t *testing.T) {
 	ConfigTestInit("../..")
 	assert.Equal(t, EnvEnumTesting, Config.ENV, "env is not set to testing")
