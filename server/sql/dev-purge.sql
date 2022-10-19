fd6ec94b25aa0ebfb9ae0b577b9975bfdcba3ad9
Lucian I. Last <li@last.nl>
2022-10-19 17:11:45 +0200

    Add sql dev-purge example

---
+++ server/sql/dev-purge.sql
@@ -0,0 +1,14 @@
+DELETE FROM user_chains;
+DELETE FROM user_tokens;
+DELETE FROM chains;
+DELETE FROM mails;
+DELETE FROM newsletters;
+DELETE FROM payments;
+DELETE FROM users;
+
+ALTER TABLE clothingloop.chains AUTO_INCREMENT=1;
+ALTER TABLE clothingloop.mails AUTO_INCREMENT=1;
+ALTER TABLE clothingloop.newsletters AUTO_INCREMENT=1;
+ALTER TABLE clothingloop.payments AUTO_INCREMENT=1;
+ALTER TABLE clothingloop.user_chains AUTO_INCREMENT=1;
+ALTER TABLE clothingloop.users AUTO_INCREMENT=1;
