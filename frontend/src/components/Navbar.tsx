09abe6b1cb22471d9872c43c489b59c29393019a
Lucian I. Last <li@last.nl>
2022-11-06 01:15:01 +0100

    Move navbar to tailwind

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useContext } from "react";
 
-import { AppBar, Button, Typography } from "@mui/material";
 import { OpenInNew } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 // Project resources
@@ -24,80 +24,78 @@ function Navbar() {
     // Use sticky position to make content start below the Navbar, instead of being covered by it.
     // Note: Not supported by IE 11. See https://material-ui.com/components/app-bar/#fixed-placement
 
-    <div>
-      <AppBar position="sticky" className={classes.header}>
-        <Link
-          to="/"
-          className={classes.logo}
-          style={
-            location.pathname === "/"
-              ? { width: "220px", height: "150px" }
-              : { width: "150px", height: "100px" }
-          }
-        >
-          <img src={Logo} alt="Logo" />
-        </Link>
-        <div className={classes.headerRight}>
-          <div className={classes.headerNav}>
-            {["/loops/find", "/"].indexOf(location.pathname) !== -1 && (
-              <Button
-                color="inherit"
-                component={Link}
-                to="/loops/new/users/signup"
-                className={`${classes.buttonCta} ${classes.buttonCtaHeader}`}
-              >
-                {t("startNewLoop")}
-              </Button>
-            )}
+    <div className="tw-container tw-mx-auto tw-z-10 tw-bg-white tw-flex tw-flex-row tw-items-center tw-py-0 tw-px-20 tw-shadow-none tw-sticky">
+      <Link
+        to="/"
+        className={classes.logo}
+        style={
+          location.pathname === "/"
+            ? { width: "220px", height: "150px" }
+            : { width: "150px", height: "100px" }
+        }
+      >
+        <img src={Logo} alt="Logo" />
+      </Link>
+      <div className={classes.headerRight}>
+        <div className={classes.headerNav}>
+          {["/loops/find", "/"].indexOf(location.pathname) !== -1 && (
+            <Button
+              color="inherit"
+              component={Link}
+              to="/loops/new/users/signup"
+              className={`${classes.buttonCta} ${classes.buttonCtaHeader}`}
+            >
+              {t("startNewLoop")}
+            </Button>
+          )}
 
-            {authUser === null &&
-            ["/loops/find", "/"].indexOf(location.pathname) === -1 ? (
-              <Button
-                color="inherit"
-                component={Link}
-                to="/loops/find"
-                className={`${classes.buttonCta} ${classes.buttonCtaHeader}`}
-              >
-                {t("findLoops")}
-                <img src={ArrowIcon} className="tw-ml-5" />
-              </Button>
-            ) : null}
+          {authUser === null &&
+          ["/loops/find", "/"].indexOf(location.pathname) === -1 ? (
+            <Button
+              color="inherit"
+              component={Link}
+              to="/loops/find"
+              className={`${classes.buttonCta} ${classes.buttonCtaHeader}`}
+            >
+              {t("findLoops")}
+              <img src={ArrowIcon} className="tw-ml-5" />
+            </Button>
+          ) : null}
 
-            {authUser && (
-              <Typography
-                color="inherit"
-                component={Link}
-                to="/admin/dashboard"
-                className={classes.buttonText}
-              >
-                {t("account")}
-              </Typography>
-            )}
-
+          {authUser && (
             <Typography
               color="inherit"
               component={Link}
-              to={authUser ? "/users/logout" : "/users/login"}
+              to="/admin/dashboard"
               className={classes.buttonText}
             >
-              {authUser ? t("logout") : t("login")}
+              {t("account")}
             </Typography>
+          )}
 
-            {authUser === null && (
-              <Typography
-                color="inherit"
-                component={Link}
-                to="/about"
-                className={classes.buttonText}
-              >
-                {t("about")}
-              </Typography>
-            )}
+          <Typography
+            color="inherit"
+            component={Link}
+            to={authUser ? "/users/logout" : "/users/login"}
+            className={classes.buttonText}
+          >
+            {authUser ? t("logout") : t("login")}
+          </Typography>
 
-            <LanguageSwitcher />
-          </div>
+          {authUser === null && (
+            <Typography
+              color="inherit"
+              component={Link}
+              to="/about"
+              className={classes.buttonText}
+            >
+              {t("about")}
+            </Typography>
+          )}
+
+          <LanguageSwitcher />
         </div>
-      </AppBar>
+      </div>
     </div>
   );
 }
