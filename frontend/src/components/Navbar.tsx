fa8cfc72dda9d0034e266993f779fae4794e9125
Lucian I. Last <li@last.nl>
2022-11-07 12:12:41 +0100

    Move navbar to daisyui

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,19 +2,13 @@ import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { useContext } from "react";
 
-import { Button, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-
 // Project resources
 import LanguageSwitcher from "./LanguageSwitcher";
 import { AuthContext } from "../providers/AuthProvider";
-import theme from "../util/theme";
-import ArrowIcon from "../images/right-arrow-yellow.svg";
 import Logo from "../images/logos/The_Clothing_Loop_Logo.png";
 
 function Navbar() {
   const { t } = useTranslation();
-  const classes = makeStyles(theme as any)();
   const { authUser } = useContext(AuthContext);
 
   let location = useLocation();
@@ -25,8 +19,9 @@ function Navbar() {
 
     <div className="tw-container tw-mx-auto tw-z-10 tw-bg-white tw-flex tw-flex-row tw-items-center tw-py-0 tw-px-20 tw-shadow-none tw-sticky">
       <Link
+        aria-label="Clothing Loop logo"
         to="/"
-        className=" tw-h-auto tw-text-4xl tw-leading-7 tw-relative tw-text-black tw-uppercase tw-w-full"
+        className="tw-h-auto tw-relative tw-w-full"
         style={
           location.pathname === "/"
             ? { width: "220px", height: "150px" }
@@ -42,58 +37,42 @@ function Navbar() {
       <div className="tw-flex tw-items-center tw-justify-end tw-flex-grow">
         <div className="tw-flex tw-items-center tw-min-h-[4rem]">
           {["/loops/find", "/"].indexOf(location.pathname) !== -1 && (
-            <Button
-              color="inherit"
-              component={Link}
+            <Link
               to="/loops/new/users/signup"
-              className={`${classes.buttonCta} ${classes.buttonCtaHeader}`}
+              className="tw-btn tw-btn-primary tw-btn-outline"
             >
               {t("startNewLoop")}
-            </Button>
+            </Link>
           )}
 
           {authUser === null &&
           ["/loops/find", "/"].indexOf(location.pathname) === -1 ? (
-            <Button
-              color="inherit"
-              component={Link}
+            <Link
               to="/loops/find"
-              className={`${classes.buttonCta} ${classes.buttonCtaHeader}`}
+              className="tw-ml-4 tw-btn tw-btn-primary tw-btn-outline"
             >
               {t("findLoops")}
-              <img src={ArrowIcon} className="tw-ml-5" />
-            </Button>
+              <span className="feather feather-arrow-right tw-ml-4"></span>
+            </Link>
           ) : null}
 
           {authUser && (
-            <Typography
-              color="inherit"
-              component={Link}
-              to="/admin/dashboard"
-              className={classes.buttonText}
-            >
+            <Link to="/admin/dashboard" className="tw-ml-4 tw-btn tw-btn-ghost">
               {t("account")}
-            </Typography>
+            </Link>
           )}
 
-          <Typography
-            color="inherit"
-            component={Link}
+          <Link
             to={authUser ? "/users/logout" : "/users/login"}
-            className={classes.buttonText}
+            className="tw-ml-4 tw-btn tw-btn-ghost"
           >
             {authUser ? t("logout") : t("login")}
-          </Typography>
+          </Link>
 
           {authUser === null && (
-            <Typography
-              color="inherit"
-              component={Link}
-              to="/about"
-              className={classes.buttonText}
-            >
+            <Link to="/about" className="tw-ml-4 tw-btn tw-btn-ghost">
               {t("about")}
-            </Typography>
+            </Link>
           )}
 
           <LanguageSwitcher />
