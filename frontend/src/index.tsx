51deede2d3231155853400a382eb84f2f1afbd64
Lucian I. Last <li@last.nl>
2022-11-22 09:07:31 +0100

    let CRA generate tailwind css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./i18n";
-import "./tailwind.generated.css";
+import "./tailwind.css";
 
 ReactDOM.render(
   <React.StrictMode>
