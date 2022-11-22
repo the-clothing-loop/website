14648d041c086920c60608876dbcacffcb06876f
Lucian I. Last <li@last.nl>
2022-11-22 10:19:22 +0100

    Fix logging in

--- frontend/src/pages/LoginEmailFinished.tsx
+++ frontend/src/pages/LoginEmailFinished.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Redirect, useLocation, useParams } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 import { Alert } from "@mui/material";
 import { makeStyles } from "@mui/styles";
@@ -13,9 +13,9 @@ import { AuthContext } from "../providers/AuthProvider";
 const LoginEmailFinished = () => {
   const { t } = useTranslation();
   const classes = makeStyles(theme as any)();
-
-  const search = useLocation().search;
-  const apiKey = new URLSearchParams(search).get("apiKey");
+  const history = useHistory();
+  const location = useLocation();
+  const apiKey = new URLSearchParams(location.search).get("apiKey");
   const { authUser, authLoginValidate } = useContext(AuthContext);
 
   const [finished, setFinished] = useState(false);
@@ -23,13 +23,25 @@ const LoginEmailFinished = () => {
 
   useEffect(() => {
     (async () => {
+      if (!!authUser) {
+        setTimeout(() => {
+          history.replace("/admin/dashboard");
+        }, 1700);
+        return;
+      }
+
       try {
         if (!apiKey) {
           throw "apiKey does not exist";
         }
         await authLoginValidate(apiKey!);
+
+        setTimeout(() => {
+          history.replace("/admin/dashboard");
+        }, 1700);
       } catch (e: any) {
-        setError(e?.data || typeof e == "string" ? e : JSON.stringify(e));
+        console.error(e);
+        setError(e?.data || typeof e === "string" ? e : JSON.stringify(e));
       }
       setFinished(true);
     })();
@@ -41,23 +53,19 @@ const LoginEmailFinished = () => {
         <title>The Clothing Loop | Login finishing</title>
         <meta name="description" content="Login finishing" />
       </Helmet>
-      {finished ||
-        (authUser && (
-          <div>
-            <Alert className={classes.successAlert} severity="success">
-              {t("userIsLoggedIn")}
-            </Alert>
-            <Redirect to="/admin/dashboard" />
-          </div>
-        ))}
       {error && (
         <Alert className={classes.errorAlert} severity="error">
           {t("errorLoggingIn")}: {error}
         </Alert>
       )}
-      {!finished && !error && (
+      {finished && !error && (
         <Alert className={classes.infoAlert} severity="info">
           {t("finishingLoggingIn")}
+        </Alert>
+      )}
+      {authUser && (
+        <Alert className={classes.successAlert} severity="success">
+          {t("userIsLoggedIn")}
         </Alert>
       )}
     </>
