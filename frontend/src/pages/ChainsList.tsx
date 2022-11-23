deaeb0ebc14718ebc3dcc788de0fa776ff774074
Lucian I. Last <li@last.nl>
2022-11-23 10:18:16 +0100

    Style loading in chainlist

--- frontend/src/pages/ChainsList.tsx
+++ frontend/src/pages/ChainsList.tsx
@@ -28,7 +28,6 @@ const ChainsList = () => {
             let data = await Promise.all(
               authUser.chains.map((c) => chainGet(c.chain_uid))
             );
-
             _chains = data.map((d) => d.data);
           }
           setChains(_chains.sort((a, b) => a.name.localeCompare(b.name)));
@@ -47,78 +46,80 @@ const ChainsList = () => {
         <meta name="description" content="Loops List" />z
       </Helmet>
       <main>
-        {chains ? (
-          <div className="tw-container tw-mx-auto">
-            <div className="tw-px-1 md:tw-px-20 tw-py-4">
-              <h1 className="tw-text-2xl tw-font-bold tw-mb-3">{`${chains.length} Clothing Loops`}</h1>
+        <div
+          className={`tw-container tw-mx-auto ${
+            chains ? "" : "tw-animate-pulse"
+          }`}
+        >
+          <div className="tw-px-1 md:tw-px-20 tw-py-4">
+            <h1 className="tw-text-2xl tw-font-bold tw-mb-3">{`${
+              chains?.length || 0
+            } Clothing Loops`}</h1>
+            {chains ? (
               <DataExport chains={chains} />
-            </div>
+            ) : (
+              <button className="tw-btn tw-btn-outline tw-btn-primary" disabled>
+                ...
+              </button>
+            )}
+          </div>
 
-            <div className="sm:tw-overflow-x-auto">
-              <table className="tw-table tw-table-compact tw-w-full">
-                <thead>
-                  <tr>
-                    <th align="left">{t("name")}</th>
-                    <th align="left">{t("location")}</th>
-                    <th align="center">{t("status")}</th>
-                    <th align="right" />
-                  </tr>
-                </thead>
-                <tbody>
-                  {chains
-                    .sort((a, b) => a.name.localeCompare(b.name))
-                    .map((chain, i) => {
-                      let isUserAdmin =
-                        authUser?.chains.find(
-                          (uc) => uc.chain_uid === chain.uid
-                        )?.is_chain_admin || false;
-                      return (
-                        <tr key={chain.name}>
-                          <td className="tw-font-bold tw-w-32 tw-whitespace-normal">
-                            {chain.name}
-                          </td>
-                          <td align="left" className="tw-whitespace-normal">
-                            {chain.address}
-                          </td>
-                          <td align="center">
-                            {chain.published ? (
-                              <div className="tw-tooltip" data-tip="published">
-                                <span className="feather feather-eye  tw-text-lg tw-text-green" />
-                              </div>
-                            ) : (
-                              <div className="tw-tooltip" data-tip="draft">
-                                <span className="feather feather-eye-off  tw-text-lg tw-text-red" />
-                              </div>
-                            )}
-                          </td>
-                          <td align="right">
-                            {(isUserAdmin || authUser?.is_root_admin) && (
-                              <Link
-                                className={`tw-btn tw-btn-primary tw-flex-nowrap ${
-                                  chains?.length > 5 ? "tw-btn-sm" : ""
-                                }`}
-                                to={`/loops/${chain.uid}/members`}
-                              >
-                                {t("view")}
-                                <span className="feather feather-arrow-right tw-ml-3"></span>
-                              </Link>
-                            )}
-                          </td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
-            </div>
+          <div className="sm:tw-overflow-x-auto">
+            <table className="tw-table tw-table-compact tw-w-full">
+              <thead>
+                <tr>
+                  <th align="left">{t("name")}</th>
+                  <th align="left">{t("location")}</th>
+                  <th align="center">{t("status")}</th>
+                  <th align="right" />
+                </tr>
+              </thead>
+              <tbody>
+                {chains
+                  ?.sort((a, b) => a.name.localeCompare(b.name))
+                  .map((chain, i) => {
+                    let isUserAdmin =
+                      authUser?.chains.find((uc) => uc.chain_uid === chain.uid)
+                        ?.is_chain_admin || false;
+                    return (
+                      <tr key={chain.name}>
+                        <td className="tw-font-bold tw-w-32 tw-whitespace-normal">
+                          {chain.name}
+                        </td>
+                        <td align="left" className="tw-whitespace-normal">
+                          {chain.address}
+                        </td>
+                        <td align="center">
+                          {chain.published ? (
+                            <div className="tw-tooltip" data-tip="published">
+                              <span className="feather feather-eye  tw-text-lg tw-text-green" />
+                            </div>
+                          ) : (
+                            <div className="tw-tooltip" data-tip="draft">
+                              <span className="feather feather-eye-off  tw-text-lg tw-text-red" />
+                            </div>
+                          )}
+                        </td>
+                        <td align="right">
+                          {(isUserAdmin || authUser?.is_root_admin) && (
+                            <Link
+                              className={`tw-btn tw-btn-primary tw-flex-nowrap ${
+                                chains?.length > 5 ? "tw-btn-sm" : ""
+                              }`}
+                              to={`/loops/${chain.uid}/members`}
+                            >
+                              {t("view")}
+                              <span className="feather feather-arrow-right tw-ml-3"></span>
+                            </Link>
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
           </div>
-        ) : (
-          <div className="tw-flex tw-items-center">
-            <span className="tw-block tw-text-xl feather feather-spinner tw-animate-spin"></span>
-            <p className="tw-text-lg tw-font-bold">
-              {t("loadingListOfAllLoops")}
-            </p>
-          </div>
-        )}
+        </div>
       </main>
     </>
   );
