f468278e1f3125b59ab781491d42d858d5b2c7f8
Lucian I. Last <li@last.nl>
2022-12-15 10:56:55 +0100

    Fix vite api connection

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/v2": "http://localhost:8084",
+      "/api": {
+        target: "http://127.0.0.1:8084",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
   },
   build: { outDir: "build" },
