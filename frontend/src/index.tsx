b881986179a285e84e095aede5d222796560845f
Lucian I. Last <li@last.nl>
2022-12-22 10:25:07 +0100

    Add mapbox css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./i18n";
 import "./airbrake";
+
+// stylesheets
 import "./tailwind.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 ReactDOM.render(
   <React.StrictMode>
