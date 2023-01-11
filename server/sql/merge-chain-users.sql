ec51f6e489740f6cabc3c706ad03ef45814e0c45
Lucian I. Last <li@last.nl>
2023-01-11 16:09:20 +0100

    Fix merge script to work with uci_user_id_chain_id

--- server/sql/merge-chain-users.sql
+++ server/sql/merge-chain-users.sql
@@ -1,39 +1,25 @@
 -- Move users from one chain to another, then delete empty chain
 
--- Move users to new chain
-UPDATE user_chains
-SET chain_id = <new>
-WHERE chain_id = <old>;
+-- Update users is_chain_admin status
+UPDATE user_chains as uc
+SET uc.is_chain_admin = TRUE
+WHERE uc.chain_id = @to
+	AND uc.user_id IN (
+		SELECT uc2.user_id
+		FROM user_chains AS uc2
+		WHERE uc2.chain_id = @from AND uc2.is_chain_admin = TRUE
+	);
 
--- Delete participant duplicate of a host
-DELETE FROM user_chains
-WHERE user_id IN (
-	SELECT uc2.user_id
-	FROM user_chains AS uc2
-	WHERE uc2.is_chain_admin = TRUE
-		AND uc2.chain_id = <new>
-   )
-	AND is_chain_admin = FALSE
-	AND chain_id = <new>;
-
--- Delete all duplicates
-DELETE uc1 FROM user_chains AS uc1
-INNER JOIN user_chains AS uc2
-WHERE uc1.id < uc2.id 
-	AND uc1.chain_id = uc2.chain_id 
-	AND uc1.user_id = uc2.user_id;
-
--- Test: find duplicates, this should return empty
-SELECT id, CONCAT(user_id, "_",chain_id) AS uniq
-FROM user_chains
-GROUP BY uniq
-HAVING COUNT(uniq) > 1;
-
--- Test: show amount of rows in old user_chains, this should return 0
-SELECT COUNT(*)
-FROM user_chains
-WHERE chain_id = <old>
+-- Move users to new chain who aren't already joined to the other chain
+UPDATE user_chains AS uc
+SET uc.chain_id = @to
+WHERE uc.chain_id = @from
+	AND uc.user_id NOT IN (
+		SELECT uc2.user_id
+		FROM user_chains as uc2
+		WHERE uc2.chain_id = @to
+	);
 
 -- Delete old chain
-DELETE FROM user_chains WHERE user_chains.chain_id = <old>;
-DELETE FROM chains WHERE chains.id = <old>;
+DELETE FROM user_chains WHERE user_chains.chain_id = @from;
+DELETE FROM chains WHERE chains.id = @from;
