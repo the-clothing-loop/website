97694f714b6277cb366a37b837d5461cbfff9247
Lucian I. Last <li@last.nl>
2023-02-01 17:40:01 +0100

    Fix error in dev-setup-users

--- server/sql/dev-setup-users.sql
+++ server/sql/dev-setup-users.sql
@@ -1,7 +1,7 @@
---- Run `dev-purge.sql` and `make generate-fake-data` before running for the best result
+-- Run `dev-purge.sql` and `make generate-fake-data` before running for the best result
 
-UPDATE users SET email = "admin@example.com", is_root_admin = TRUE WHERE id = 1;
-UPDATE users SET email = "host@example.com" WHERE id = 2;
-UPDATE user_chains SET is_chain_admin = TRUE WHERE user_id = 2;
-UPDATE users SET email = "user@example.com" WHERE id = 3;
-UPDATE user_chains SET is_chain_admin = FALSE WHERE user_id = 3;
+UPDATE users SET email = "admin@example.com", name = "Admin", is_root_admin = TRUE WHERE id = 1;
+UPDATE users SET email = "host@example.com", name = "Host" WHERE id = 2;
+UPDATE user_chains SET is_chain_admin = TRUE, is_approved = TRUE WHERE user_id = 2;
+UPDATE users SET email = "user@example.com", name = "Participant" WHERE id = 3;
+UPDATE user_chains SET is_chain_admin = FALSE, is_approved = TRUE WHERE user_id = 3;
