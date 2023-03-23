d09836e2885b32a004c2c330b2ffdd645f3efa35
Lucian I. Last <li@last.nl>
2023-03-23 10:16:29 +0100

    Add error log to user purge

--- server/internal/controllers/users.go
+++ server/internal/controllers/users.go
@@ -317,15 +317,55 @@ HAVING COUNT(uc.id) = 1
 	`, user.ID).Scan(&chainIDsToDelete)
 
 	tx := db.Begin()
-	tx.Exec(`DELETE FROM user_chains WHERE user_id = ?`, user.ID)
-	tx.Exec(`DELETE FROM user_tokens WHERE user_id = ?`, user.ID)
-	tx.Exec(`DELETE FROM users WHERE id = ?`, user.ID)
+
+	err := tx.Exec(`DELETE FROM user_chains WHERE user_id = ?`, user.ID).Error
+	if err != nil {
+		tx.Rollback()
+		goscope.Log.Errorf("UserPurge: Unable to remove loop connections: %v", err)
+		c.String(http.StatusInternalServerError, "Unable to remove loop connections")
+		return
+	}
+	err = tx.Exec(`DELETE FROM user_tokens WHERE user_id = ?`, user.ID).Error
+	if err != nil {
+		tx.Rollback()
+		goscope.Log.Errorf("UserPurge: Unable to remove token connections: %v", err)
+		c.String(http.StatusInternalServerError, "Unable to remove token connections")
+		return
+	}
+	err = tx.Exec(`DELETE FROM users WHERE id = ?`, user.ID).Error
+	if err != nil {
+		tx.Rollback()
+		goscope.Log.Errorf("UserPurge: Unable to user: %v", err)
+		c.String(http.StatusInternalServerError, "Unable to user")
+		return
+	}
+
 	if len(chainIDsToDelete) > 0 {
-		tx.Exec(`DElETE FROM chains WHERE id IN ?`, chainIDsToDelete)
+		err := tx.Exec(`DELETE FROM user_chains WHERE chain_id IN ?`, chainIDsToDelete).Error
+		if err != nil {
+			tx.Rollback()
+			goscope.Log.Errorf("UserPurge: Unable to remove hosted loop connections: %v", err)
+			c.String(http.StatusInternalServerError, "Unable to remove hosted loop connections")
+			return
+		}
+		err = tx.Exec(`DELETE FROM chains WHERE id IN ?`, chainIDsToDelete).Error
+		if err != nil {
+			tx.Rollback()
+			goscope.Log.Errorf("UserPurge: Unable to remove hosted loop: %v", err)
+			c.String(http.StatusInternalServerError, "Unable to remove hosted loop")
+			return
+		}
 	}
 
 	if user.Email.Valid {
-		tx.Exec(`DELETE FROM newsletters WHERE email = ?`, user.Email.String)
+		err = tx.Exec(`DELETE FROM newsletters WHERE email = ?`, user.Email.String).Error
+		if err != nil {
+			tx.Rollback()
+			goscope.Log.Errorf("UserPurge: Unable to remove newsletter: %v", err)
+			c.String(http.StatusInternalServerError, "Unable to remove newsletter")
+			return
+		}
 	}
+
 	tx.Commit()
 }
