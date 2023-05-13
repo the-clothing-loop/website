5f3a127675952d9be504105248a54ff75d2d1f3b
Natalie <nataliewilson519@gmail.com>
2023-05-13 11:51:14 -0700

    implement AddressForm component for UserEdit

--- frontend/src/pages/UserEdit.tsx
+++ frontend/src/pages/UserEdit.tsx
@@ -2,12 +2,6 @@ import { useState, useEffect, useMemo, FormEvent, useContext } from "react";
 import { useParams, useHistory, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet";
-import Geocoding, { Estimate } from "./Geocoding";
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-
-import SizesDropdown from "../components/SizesDropdown";
-import { TextForm } from "../components/FormFields";
-import categories from "../util/categories";
 import { UID, User } from "../api/types";
 import {
   userGetByUID,
@@ -15,11 +9,11 @@ import {
   userUpdate,
   UserUpdateBody,
 } from "../api/user";
-import { PhoneFormField } from "../components/FormFields";
 import useForm from "../util/form.hooks";
 import { ToastContext } from "../providers/ToastProvider";
 import { GinParseErrors } from "../util/gin-errors";
 import { AuthContext } from "../providers/AuthProvider";
+import AddressForm from "../components/AddressForm";
 
 interface Params {
   userUID: UID;
@@ -28,8 +22,6 @@ interface Params {
 interface State {
   chainUID?: UID;
 }
-
-const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_KEY;
 
 export default function UserEdit() {
   const { t } = useTranslation();
@@ -53,7 +45,6 @@ export default function UserEdit() {
   const [user, setUser] = useState<User>();
   const { authUser } = useContext(AuthContext);
   const params = useParams<Params>();
-  const [geocoder, setGeocoder] = useState<MapboxGeocoder>();
 
   const userUID: string =
     params.userUID == "me" ? authUser!.uid : params.userUID;
@@ -69,42 +60,17 @@ export default function UserEdit() {
     [user]
   );
 
-  function onSubmit(e: FormEvent) {
+  function onSubmit(e: FormEvent, address: string) {
     e.preventDefault();
 
     (async () => {
       try {
-        if (MAPBOX_TOKEN) {
-          const _geocoder = new MapboxGeocoder({
-            accessToken: MAPBOX_TOKEN,
-            types: "address",
-          });
-          const correctAddress =
-            address.street.replaceAll(" ", "%20") +
-            "%20" +
-            address.zip +
-            "%20" +
-            address.cityState.replaceAll(" ", "%20");
-
-          window.axios
-            .get(
-              `https://api.mapbox.com/geocoding/v5/mapbox.places/${correctAddress}.json?access_token=${MAPBOX_TOKEN}`
-            )
-            .then((response) => {
-              console.log(response.data);
-
-              //  var data = JSON(response.data);
-              var placeName = response.data.features[0].place_name;
-              console.log(placeName);
-              setValue("address", placeName);
-            });
-        }
         let userUpdateBody: UserUpdateBody = {
           user_uid: userUID,
           name: values.name,
           phone_number: values.phone,
           newsletter: values.newsletter,
-          address: values.address,
+          address: address,
           sizes: values.sizes,
         };
         if (chainUID) userUpdateBody.chain_uid = chainUID;
@@ -125,13 +91,6 @@ export default function UserEdit() {
         const user = (await userGetByUID(chainUID, userUID)).data;
         const hasNewsletter = (await userHasNewsletter(chainUID, userUID)).data;
         setUser(user);
-        setValues({
-          name: user.name,
-          phone: user.phone_number,
-          sizes: user.sizes,
-          address: user.address,
-          newsletter: hasNewsletter,
-        });
       } catch (error) {
         console.warn(error);
       }
@@ -145,8 +104,8 @@ export default function UserEdit() {
         <title>The Clothing Loop | Edit user</title>
         <meta name="description" content="Edit user" />
       </Helmet>
-      <main className="">
-        <div className="bg-teal-light w-full container sm:max-w-screen-xs mx-auto p-10">
+      <main>
+        <div className="bg-teal-light w-full container sm:max-w-screen-xs mx-auto p-6">
           <h1 className="font-sans font-semibold text-3xl text-secondary mb-4">
             {chainUID
               ? user.is_root_admin || userIsChainAdmin
@@ -154,116 +113,26 @@ export default function UserEdit() {
                 : t("editParticipantContacts")
               : t("editAccount")}
           </h1>
-          <form onSubmit={onSubmit} className="relative">
-            <div className="mb-2">
-              <TextForm
-                type="text"
-                autoComplete="name"
-                label={t("name")}
-                name="name"
-                required
-                min={2}
-                value={values.name}
-                onChange={(e) => setValue("name", e.target.value)}
-              />
-            </div>
+          <AddressForm onSubmit={onSubmit} classes="" />
+          <div className="flex">
+            <button
+              type="button"
+              onClick={() => history.goBack()}
+              className="btn btn-secondary btn-outline"
+            >
+              {t("back")}
+            </button>
 
-            <PhoneFormField
-              value={values.phone}
-              onChange={(e) => setValue("phone", e.target.value)}
-            />
-            <div className="form-control mb-4">
-              <TextForm
-                type="text"
-                label={t("streetAddress")}
-                name="streetAddress"
-                autoComplete="street-address"
-                required
-                min={2}
-                value={address.street}
-                onChange={(e) => setAddress("street", e.target.value)}
-              />
-            </div>
-
-            <div className="form-control mb-4">
-              <TextForm
-                type="text"
-                label={t("zip")}
-                name="zip"
-                autoComplete="postal-code"
-                min={2}
-                value={address.zip}
-                onChange={(e) => setAddress("zip", e.target.value)}
-              />
-            </div>
-
-            <div className="form-control mb-4">
-              <TextForm
-                type="text"
-                label={t("city")}
-                name="city"
-                autoComplete="city"
-                min={2}
-                value={address.cityState}
-                onChange={(e) => setAddress("cityState", e.target.value)}
-              />
-            </div>
-
-            <div className="form-control mb-4">
-              <TextForm
-                type="text"
-                label={t("country")}
-                name="country"
-                autoComplete="country"
-                min={2}
-                value={address.country}
-                onChange={(e) => setAddress("country", e.target.value)}
-              />
-            </div>
-
-            <div className="mb-4 pt-3">
-              <SizesDropdown
-                filteredGenders={Object.keys(categories)}
-                selectedSizes={values.sizes || []}
-                className="w-full"
-                handleChange={(v) => setValue("sizes", v)}
-              />
-            </div>
-
-            <div className="form-control mb-4">
-              <label className="label cursor-pointer">
-                <span className="label-text">
-                  {t("newsletterSubscription")}
-                </span>
-                <input
-                  type="checkbox"
-                  checked={values.newsletter}
-                  onChange={() => setValue("newsletter", !values.newsletter)}
-                  required={
-                    user.is_root_admin || userIsAnyChainAdmin ? true : false
-                  }
-                  className="checkbox"
-                  name="newsletter"
-                />
-              </label>
-            </div>
-
-            <div className="flex">
-              <button
-                type="button"
-                onClick={() => history.goBack()}
-                className="btn btn-secondary btn-outline"
-              >
-                {t("back")}
-              </button>
-
-              <button type="submit" className="btn btn-primary ml-3">
-                {t("submit")}
-                <span className="feather feather-arrow-right ml-4 rtl:hidden"></span>
-                <span className="feather feather-arrow-left mr-4 ltr:hidden"></span>
-              </button>
-            </div>
-          </form>
+            <button
+              type="submit"
+              className="btn btn-primary ml-3"
+              form="address-form"
+            >
+              {t("submit")}
+              <span className="feather feather-arrow-right ml-4 rtl:hidden"></span>
+              <span className="feather feather-arrow-left mr-4 ltr:hidden"></span>
+            </button>
+          </div>
         </div>
       </main>
     </>
