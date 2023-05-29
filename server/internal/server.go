e08c7cf7fc050306a2d4be10423b239848e8f9be
Lucian I. Last <li@last.nl>
2023-05-29 23:51:47 +0200

    First attempt in backend

--- server/internal/server.go
+++ server/internal/server.go
@@ -74,6 +74,7 @@ func Routes() *gin.Engine {
 	v2.PATCH("/user", controllers.UserUpdate)
 	v2.DELETE("/user", controllers.UserDelete)
 	v2.DELETE("/user/purge", controllers.UserPurge)
+	v2.POST("/user/transfer-chain", controllers.UserTransferChain)
 
 	// chain
 	v2.GET("/chain", controllers.ChainGet)
