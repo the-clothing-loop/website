86c8302aab210b110f499c9aadcae670822aeff9
Lucian I. Last <li@last.nl>
2023-06-29 09:48:16 +0200

    Fix language detection on mobile

--- app/src/i18n.ts
+++ app/src/i18n.ts
@@ -11,10 +11,12 @@ const i18n = i18next
     load: "languageOnly",
     ns: ["translation", "faq"],
     supportedLngs: ["en", "nl"],
-    lng: "nl",
     fallbackLng: "en",
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json",
+    },
+    detection: {
+      order: ["querystring", "navigator"],
     },
   });
 
