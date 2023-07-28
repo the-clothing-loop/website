b5d5741a67d96cedaa069de61eb4be6a4a756245
Lucian I. Last <li@last.nl>
2023-07-29 00:20:14 +0200

    Fix test cases

--- server/pkg/tsp/mst_test.go
+++ server/pkg/tsp/mst_test.go
@@ -1,18 +1,36 @@
 package tsp
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPreorder(t *testing.T) {
-	tree := [][]int{{1}, {1, 2}, {1, 2, 3}, {4}}
-	path := []int{1, 2}
-	preorder(&tree, &path, 2)
-	assert.Equal(t, []int{1, 2}, path)
+func TestToRadians(t *testing.T) {
+	// Decimal point accuracy
+	p := 1e6
+
+	radian := toRadians(360)
+	assert.Equal(t, math.Round(radian*p)/p, 6.283185)
 }
 
-func TestToRadians(t *testing.T) {
-	assert.Equal(t, toRadians(90), 25)
+func TestCalculateDistance(t *testing.T) {
+	// Paris: Lat: 48.8566° N, Long: 2.3522° E.
+	paris := City[string]{
+		Latitude:  48.8566,
+		Longitude: 2.3522,
+	}
+	// Krakow: Lat: 50.0647° N, Long: 19.9450° E.
+	krakow := City[string]{
+		Latitude:  50.0647,
+		Longitude: 19.9450,
+	}
+	expectedDistance := 1275.6
+
+	// Decimal point accuracy
+	p := 10.0
+
+	sut := calculateDistance(paris.Latitude, paris.Longitude, krakow.Latitude, krakow.Longitude)
+	assert.Equal(t, expectedDistance, math.Round(sut*p)/p)
 }
