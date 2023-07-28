32b984f250a03b632f9b3c834b33669bd753b750
Lucian I. Last <li@last.nl>
2023-07-28 09:29:58 +0200

    Add im buttons in address item page

--- app/src/pages/AddressItem.tsx
+++ app/src/pages/AddressItem.tsx
@@ -5,12 +5,55 @@ import {
   IonPage,
   IonBackButton,
   IonButtons,
+  IonFabButton,
+  IonImg,
 } from "@ionic/react";
 import { useContext, useMemo } from "react";
 import { RouteComponentProps } from "react-router";
 import UserCard from "../components/UserCard";
 import { StoreContext } from "../Store";
 import isPaused from "../utils/is_paused";
+
+interface MessagingApp {
+  icon: string;
+  name: string;
+  color: string;
+  colorTint: string;
+  colorFade: string;
+}
+const messagingApps: MessagingApp[] = [
+  {
+    icon: "/icons/sms.svg",
+    name: "Sms",
+    color: "#44e75f",
+    colorTint: "#5dfc77",
+    colorFade: "#1dc93a",
+  },
+  {
+    icon: "/icons/whatsapp.svg",
+    name: "WhatsApp",
+
+    color: "#25d366",
+    colorTint: "#73f793",
+    colorFade: "#128c7e",
+  },
+  {
+    icon: "/icons/telegram.svg",
+    name: "Telegram",
+
+    color: "#29a9eb",
+    colorTint: "#7eb9e1",
+    colorFade: "#0f86d7",
+  },
+  {
+    icon: "/icons/signal.svg",
+    name: "Signal",
+
+    color: "#3a76f0",
+    colorTint: "#2f6ded",
+    colorFade: "#3c3744",
+  },
+];
 
 export default function AddressItem({
   match,
@@ -35,6 +78,31 @@ export default function AddressItem({
         {user ? (
           <UserCard user={user} chain={chain} isUserPaused={isUserPaused} />
         ) : null}
+        <div
+          style={{ display: "flex" }}
+          className="ion-margin-start ion-margin-end ion-margin-top"
+        >
+          {messagingApps.map((app) => (
+            <IonFabButton
+              key={app.name}
+              style={{
+                marginInlineEnd: 8,
+                "--background": app.color,
+                "--background-activated": app.colorFade,
+                "--background-focused": app.colorFade,
+                "--background-hover": app.colorTint,
+              }}
+            >
+              <IonImg
+                src={app.icon}
+                alt={app.name}
+                style={{
+                  margin: 12,
+                }}
+              />
+            </IonFabButton>
+          ))}
+        </div>
       </IonContent>
     </IonPage>
   );
