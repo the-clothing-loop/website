b37e503a35ebbaf460a810f27e9ff4639ed1349f
Lucian I. Last <li@last.nl>
2023-08-16 08:54:08 +0200

    remove screens

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -63,13 +63,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   prefix: "tw-",
   theme: {
-    screens: {
-      xs: "481px",
-      sm: "600px",
-      md: "900px",
-      lg: "1200px",
-      xl: "1536px",
-    },
+    screens: {},
     colors,
     fontFamily: {
       sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
