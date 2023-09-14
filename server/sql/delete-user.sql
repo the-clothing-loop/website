7bfd6d378874148d55b9a33de131f9e2d4ba123a
Lucian I. Last <li@last.nl>
2023-09-14 17:48:48 +0200

    Update delete user

--- server/sql/delete-user.sql
+++ server/sql/delete-user.sql
@@ -9,4 +9,8 @@ DELETE FROM user_chains WHERE user_chains.user_id = 0;
 
 DELETE FROM user_tokens WHERE user_tokens.user_id = 0;
 
+UPDATE events SET user_id = (
+    SELECT id FROM users WHERE is_root_admin = 1 LIMIT 1
+) WHERE user_id = 0;
+
 DELETE FROM users WHERE users.id = 0;
