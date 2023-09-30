366fc1717fa99149ebf6550a4a54071eb5cac054
Natalie <nataliewilson519@gmail.com>
2023-09-30 12:20:19 -0700

    add mobile formatting

--- frontend/src/pages/Contribute.tsx
+++ frontend/src/pages/Contribute.tsx
@@ -36,8 +36,8 @@ export default function Contribute() {
           <h1 className="font-serif font-bold text-secondary text-4xl md:text-6xl mb-16">
             {t("howToContribute")}
           </h1>
-          <div className="flex items-center">
-            <div className="w-1/2">
+          <div className="flex flex-col md:flex-row items-center">
+            <div className="w-full md:w-1/2 mb-8 md:mb-0">
               <p className="prose font-serif text-2xl font-bold text-secondary -mt-8 mb-4">
                 <Trans i18nKey="startALoop" ns="contribute" />
               </p>
@@ -45,18 +45,18 @@ export default function Contribute() {
                 <Trans i18nKey="startALoopDesc" ns="contribute" />
               </p>
             </div>
-            <Link className="w-1/2 pl-12" to="/loops/find" target="_blank">
+            <Link className="w-full md:w-1/2 md:pl-12" to="/loops/find" target="_blank">
               <img
                 src={map}
-                alt="map of the clothign loop in Amsterdam area"
+                alt="map of the clothing loop in Amsterdam area"
                 className="object-cover hover:ring-[1.5rem] ring-secondary transition-[box-shadow]"
               />
             </Link>
           </div>
 
-          <div className="flex flex-col md:flex-row md:items-center md:mb-16 my-12 items-center content-center">
-            <div className="flex flex-col items-center w-2/3 min-w-[350px] md:w-1/3 md:h-80 prose font-serif font-bold bg-teal-light text-center px-8 md:px-0 py-6 md:py-12 mx-auto">
-              <h2 className="font-serif font-bold text-lg md:text-lg py-0 my-auto">
+          <div className="flex flex-col-reverse md:flex-row md:items-center md:mb-16 my-12 items-center content-center">
+            <div className="p-12 md:p-8 flex flex-col items-center w-2/3 md:w-1/3 min-w-[350px] prose font-serif font-bold bg-teal-light text-center">
+              <h2 className="font-serif font-bold text-lg md:text-lg py-0 my-auto mb-8">
                 <span className="inline md:block text-5xl font-bold font-secondary">
                   {t("findUs")}
                 </span>
@@ -80,7 +80,6 @@ export default function Contribute() {
                       className="btn btn-circle btn-outline feather feather-instagram text-lg font-bold bg-white hover:bg-instagram !no-underline"
                       aria-label="link to our instagram account"
                     ></a>
-                  
                   </li>
                   <li className="list-none px-0 w-12">
                     <a
@@ -105,11 +104,11 @@ export default function Contribute() {
                 </ul>
               </div>
             </div>
-            <div className="md:ml-8 w-full md:w-2/3">
+            <div className="md:ml-8 w-full md:w-2/3 mb-8 md:mb-0">
               <p className="prose font-serif text-2xl font-bold text-secondary mb-4">
                 <Trans i18nKey="shareSwapStory" ns="contribute" />
               </p>
-              <p className="prose font-serif text-lg font-normal mb-4">
+              <p className="prose font-serif text-lg font-normal mb-8 md:mb-4">
                 <Trans
                   i18nKey="shareSwapStoryDesc"
                   ns="contribute"
@@ -156,8 +155,8 @@ export default function Contribute() {
             </div>
           </div>
 
-          <div className="flex items-center mb-12">
-            <div className="w-1/2">
+          <div className="flex flex-col md:flex-row items-center mb-8 md:mb-4">
+            <div className="w-full md:w-1/2 md:pr-8 mb-4 md:mb-0">
               <p className="prose font-serif text-2xl font-bold text-secondary mb-4">
                 <Trans i18nKey="swap" ns="contribute" />
               </p>
@@ -178,30 +177,30 @@ export default function Contribute() {
                 />
               </p>
             </div>
-            <Link className="w-1/2" to="/events" target="_blank">
+            <Link className="w-2/3 md:w-1/2" to="/events" target="_blank">
               <img
                 src={events}
-                alt="events page"
+                alt="screenshot of the events page"
                 className="object-cover hover:ring-[1.5rem] ring-secondary transition-[box-shadow]"
               />
             </Link>
           </div>
 
-          <div className="flex items-center mt-12">
+          <div className="flex flex-col-reverse md:flex-row items-center mb-8 md:mb-4">
             <a
-              className="w-[40%] pr-12"
+              className="w-2/3 md:w-1/3 md:pr-8 max-w-md "
               href="https://crowdin.com/project/the-clothing-loop"
               target="_blank"
             >
               <img
                 src={crowdin}
-                alt="crowdin"
+                alt="the different languages we have on Crowdin"
                 className="object-cover hover:ring-[1.5rem] ring-secondary transition-[box-shadow]"
               />
             </a>
 
-            <div className="w-[60%] self-center">
-              <p className="prose font-serif text-2xl font-bold text-secondary -mt-8">
+            <div className="w-full md:w-2/3 self-center mb-8 md:mb-0">
+              <p className="prose font-serif text-2xl font-bold text-secondary mb-4">
                 <Trans
                   i18nKey="crowdin"
                   ns="contribute"
@@ -217,7 +216,7 @@ export default function Contribute() {
                   }}
                 />
               </p>
-              <p className="prose font-serif text-lg font-normal mb-4">
+              <p className="prose font-serif text-lg font-normal">
                 <Trans
                   i18nKey="crowdinDesc"
                   ns="contribute"
@@ -235,60 +234,64 @@ export default function Contribute() {
               </p>
             </div>
           </div>
-          <p className="prose font-serif text-2xl font-bold text-secondary mb-4">
-            <Trans i18nKey="website" ns="contribute" />
-          </p>
-          <p className="prose font-serif text-lg font-normal mb-4">
-            <Trans
-              i18nKey="websiteDesc"
-              ns="contribute"
-              components={{
-                p: <p></p>,
-                aGithub: (
-                  <a
-                    className="link"
-                    href="https://github.com/the-clothing-loop/website/issues"
-                    target="_blank"
-                  />
-                ),
-                aEmail: (
-                  <a
-                    className="link"
-                    href="mailto:hello@clothingloop.org"
-                    aria-label="Our email address"
-                  ></a>
-                ),
-              }}
-            />
-          </p>
-          <p className="prose font-serif text-2xl font-bold text-secondary mt-4">
-            <Trans
-              i18nKey="donate"
-              ns="contribute"
-              components={{
-                p: <p></p>,
-                aDonate: (
-                  <Link
-                    className="prose font-serif text-2xl font-bold text-secondary"
-                    to="/donate"
-                    target="_blank"
-                  ></Link>
-                ),
-              }}
-            />
-          </p>
-          <p className="prose font-serif text-lg font-normal mb-4">
-            <Trans
-              i18nKey="donateDesc"
-              ns="contribute"
-              components={{
-                p: <p></p>,
-                aDonate: (
-                  <Link className="link" to="/donate" target="_blank"></Link>
-                ),
-              }}
-            />
-          </p>
+          <div className="mb-4">
+            <p className="prose font-serif text-2xl font-bold text-secondary mb-4">
+              <Trans i18nKey="website" ns="contribute" />
+            </p>
+            <p className="prose font-serif text-lg font-normal">
+              <Trans
+                i18nKey="websiteDesc"
+                ns="contribute"
+                components={{
+                  p: <p></p>,
+                  aGithub: (
+                    <a
+                      className="link"
+                      href="https://github.com/the-clothing-loop/website/issues"
+                      target="_blank"
+                    />
+                  ),
+                  aEmail: (
+                    <a
+                      className="link"
+                      href="mailto:hello@clothingloop.org"
+                      aria-label="Our email address"
+                    ></a>
+                  ),
+                }}
+              />
+            </p>
+          </div>
+          <div className="mb-8">
+            <p className="prose font-serif text-2xl font-bold text-secondary mb-4">
+              <Trans
+                i18nKey="donate"
+                ns="contribute"
+                components={{
+                  p: <p></p>,
+                  aDonate: (
+                    <Link
+                      className="prose font-serif text-2xl font-bold text-secondary"
+                      to="/donate"
+                      target="_blank"
+                    ></Link>
+                  ),
+                }}
+              />
+            </p>
+            <p className="prose font-serif text-lg font-normal mb-4">
+              <Trans
+                i18nKey="donateDesc"
+                ns="contribute"
+                components={{
+                  p: <p></p>,
+                  aDonate: (
+                    <Link className="link" to="/donate" target="_blank"></Link>
+                  ),
+                }}
+              />
+            </p>
+          </div>
         </div>
       </main>
     </>
