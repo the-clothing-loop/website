b187360f76159bd5fa2a0e3081d9fe4c117f1f7d
Tim Fierens <tim@tobifaf.nl>
2023-10-11 22:18:22 +0200

    use the one of the actual hosts to set the i18n

--- server/sql/prepend-i18n-bag-name.sql
+++ server/sql/prepend-i18n-bag-name.sql
@@ -1,23 +1,20 @@
-UPDATE
-    bags b
-        JOIN user_chains uc ON
-        b.user_chain_id = uc.id
-        JOIN users u ON
-        uc.user_id = u.id
+UPDATE bags b
+    JOIN user_chains bag_chain ON 
+        b.user_chain_id = bag_chain.id
+    JOIN user_chains uc ON
+        bag_chain.chain_id = uc.chain_id AND uc.is_chain_admin = 1
+    JOIN users u ON
+        uc.user_id = u.id AND LENGTH(u.i18n) = 2
 SET b.number = IF(
-                LENGTH(b.number) < 7 AND (
-                (
-                    u.i18n = 'nl' AND LEFT(b.number, 4) != 'Tas '
-                    ) OR (
-                    u.i18n = 'fr' AND LEFT(b.number, 4) != 'Sac '
-                    ) OR (
-                    u.i18n NOT IN ('nl', 'fr') AND LEFT(b.number, 4) != 'Bag '
-                    )
-            ),
-                CASE
-                    WHEN u.i18n = 'nl' THEN CONCAT('Tas ', b.number)
-                    WHEN u.i18n = 'fr' THEN CONCAT('Sac ', b.number)
-                    ELSE CONCAT('Bag ', b.number)
-                    END,
-                b.number
-    );
+    LENGTH(b.number) < 7 AND (
+        (u.i18n = 'nl' AND LEFT(b.number, 4) != 'Tas ') OR 
+        (u.i18n = 'fr' AND LEFT(b.number, 4) != 'Sac ') OR 
+        (u.i18n NOT IN ('nl', 'fr') AND LEFT(b.number, 4) != 'Bag ')
+    ),
+        CASE
+            WHEN u.i18n = 'nl' THEN CONCAT('Tas ', b.number)
+            WHEN u.i18n = 'fr' THEN CONCAT('Sac ', b.number)
+            ELSE CONCAT('Bag ', b.number)
+        END,
+        b.number
+);
