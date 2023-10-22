db7229fb4ac5129b029c84afe3597ad4b939668b
Natalie <nataliewilson519@gmail.com>
2023-10-22 14:33:06 -0700

    New onboarding page UX

--- app/src/pages/Onboarding.tsx
+++ app/src/pages/Onboarding.tsx
@@ -16,34 +16,30 @@ import {
 } from "ionicons/icons";
 import { useHistory } from "react-router";
 
+const logo = "https://images.clothingloop.org/x512/the_clothing_loop_logo.png";
+const CirclesFrame = "https://images.clothingloop.org/0x0/circles.png";
+
 export function OnboardingPageOne() {
   const history = useHistory();
   return (
     <IonPage>
-      <IonContent color="primary">
-        <div className="tw-flex tw-justify-center tw-items-center tw-flex-col tw-h-full">
-          <IonText className="tw-text-center tw-font-bold tw-text-xl tw-leading-7 tw-font-sans tw-mb-10">
-            Welcome to
-            <br /> the Clothing Loop's app
-          </IonText>
-          <IonImg
-            className="tw-w-[70%] tw-max-w-[400px]"
-            src="https://images.clothingloop.org/426x/paloeka_nichon_landscape.jpg"
-          />
-          <IonText className="tw-text-center tw-font-bold tw-text-3xl tw-leading-10 tw-font-serif tw-my-10">
-            Locate bags
-            <br /> and pass them on!
-          </IonText>
-          <IonButton
-            shape="round"
-            color="danger"
-            target="_blank"
-            href="https://www.clothingloop.org/about"
+      <IonContent>
+        <IonImg
+          src={logo}
+          className="tw-w-full tw-px-8 tw-h-auto tw-mx-auto tw-pt-12"
+        />
+
+        <div className="tw-flex tw-justify-center">
+          <h1
+            className="tw-text-secondary tw-fixed tw-inset-y-[45%] tw-font-serif tw-font-bold tw-text-accent tw-text-7xl tw-md:text-9xl tw-mb-8 tw-rtl:text-end"
+            dir="ltr"
           >
-            Who are we?
-            <IonIcon icon={compassOutline} className="ion-margin-start" />
-          </IonButton>
+            Swap, <br />
+            <IonText className="tw-text-stroke-accent">don't shop</IonText>
+          </h1>
         </div>
+        <div className="tw-bg-primary-shade tw-fixed tw-bottom-0 tw-h-[45%] tw-w-full -tw-z-10" />
+        <IonImg src={CirclesFrame} className="tw-fixed tw-bottom-0" />
 
         <IonFab vertical="bottom" horizontal="end">
           <IonFabButton
