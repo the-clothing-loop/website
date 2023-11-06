cdb393313ee2a488ad44142eddbdbad046278093
Lucian I. Last <li@last.nl>
2023-11-06 10:25:52 +0100

    Improve legibility of SQL scripts

--- server/internal/models/user.go
+++ server/internal/models/user.go
@@ -153,13 +153,17 @@ func UserGetByEmail(db *gorm.DB, userEmail string) (*User, error) {
 func UserGetAdminsByChain(db *gorm.DB, chainId ...uint) ([]UserContactData, error) {
 	results := []UserContactData{}
 	err := db.Raw(`
-	SELECT users.name AS name, users.email AS email, users.i18n AS i18n, chains.name as chain_name
-	FROM user_chains AS uc
-	LEFT JOIN users ON uc.user_id = users.id
-	LEFT JOIN chains ON uc.chain_id = chains.id 
-	WHERE uc.chain_id IN ?
-		AND uc.is_chain_admin = TRUE
-		AND users.is_email_verified = TRUE
+SELECT
+	users.name AS name,
+	users.email AS email,
+	users.i18n AS i18n,
+	chains.name AS chain_name
+FROM user_chains AS uc
+LEFT JOIN users ON uc.user_id = users.id
+LEFT JOIN chains ON uc.chain_id = chains.id 
+WHERE uc.chain_id IN ?
+	AND uc.is_chain_admin = TRUE
+	AND users.is_email_verified = TRUE
 	`, chainId).Scan(&results).Error
 	if err != nil {
 		return nil, err
@@ -171,10 +175,10 @@ func UserGetAllUsersByChain(db *gorm.DB, chainID uint) ([]User, error) {
 	results := []User{}
 
 	err := db.Raw(`
-	SELECT users.*
-	FROM users
-	LEFT JOIN user_chains ON user_chains.user_id = users.id 
-	WHERE user_chains.chain_id = ? AND users.is_email_verified = TRUE
+SELECT users.*
+FROM users
+LEFT JOIN user_chains ON user_chains.user_id = users.id 
+WHERE user_chains.chain_id = ? AND users.is_email_verified = TRUE
 	`, chainID).Scan(&results).Error
 
 	if err != nil {
