34ee845337e0cdfbd03820de5b4776d8221daa30
Lucian I. Last <li@last.nl>
2023-11-18 11:37:50 +0100

    Remove unused imports

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,13 @@
-// Material
+import { useRef } from "react";
 import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
 import { Link, useHistory } from "react-router-dom";
 
-// Project resources
 import Counters from "../components/Counters/Counters";
 import Carousel from "../components/Carousel";
 import Testimonials from "../components/Testimonials";
 import StandaloneSearchBar from "../components/FindChain/StandaloneSearchBar";
 import useIntersectionObserver from "../components/Counters/hooks";
-import { useRef } from "react";
 
 const CirclesFrame = "https://images.clothingloop.org/0x0/circles.png";
 
