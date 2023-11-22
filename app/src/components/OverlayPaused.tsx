38b3cc98eb58718ede0f814a860e1783020dfa17
Lucian I. Last <li@last.nl>
2023-11-22 14:06:12 +0100

    Fix overlay paused

--- app/src/components/OverlayPaused.tsx
+++ app/src/components/OverlayPaused.tsx
@@ -43,64 +43,62 @@ export default function OverlayPaused() {
 
   if (isExplicitlyPublished || !isOverlayPausedOpen) return null;
   return (
-    <IonPage>
-      <div className="tw-absolute tw-inset-0">
-        <div className="tw-relative tw-z-10 tw-flex tw-h-full tw-flex-col tw-justify-center tw-items-center tw-text-center">
-          <IonCard className="tw-bg-background">
-            <IonCardContent className="tw-text-text">
-              <h1 className="!tw-mb-4">{t("loopIsNotPublished")}</h1>
-              {isUserHost ? null : (
-                <>
-                  <p className="!tw-mb-3">{t("pleaseContactYourLoopHost")}</p>
-                  <div className="tw-flex tw-justify-center tw-flex-wrap tw-mt-1.5 tw-m-0 tw-mb-2.5">
-                    {hosts.map((host) => (
-                      <IonButton
-                        key={host.uid}
-                        size="small"
-                        routerLink={"/address/" + host.uid}
-                        color="light"
-                        className="tw-m-1.5 tw-text-base"
-                      >
-                        {host.name}
-                      </IonButton>
-                    ))}
-                  </div>
-                </>
-              )}
-              <IonButton
-                onClick={goToSettings}
-                expand="block"
-                color="primary"
-                fill={isUserHost ? "outline" : "solid"}
-              >
-                {t("selectADifferentLoop")}
-              </IonButton>
-              {isUserHost ? (
-                <>
-                  <IonButton
-                    href={`https://clothingloop.org/loops/${chain?.uid}/members`}
-                    target="_blank"
-                    expand="block"
-                    className="tw-mt-4"
-                  >
-                    {t("editLoop")}
-                    <IonIcon slot="end" icon={openOutline}></IonIcon>
-                  </IonButton>
-                  <IonButton
-                    expand="block"
-                    className="tw-mt-4"
-                    color="light"
-                    onClick={() => closeOverlayPaused()}
-                  >
-                    {t("hide")}
-                  </IonButton>
-                </>
-              ) : null}
-            </IonCardContent>
-          </IonCard>
-        </div>
-        <div className="tw-absolute tw-z-0 tw-inset-0 tw-bg-paused tw-opacity-20"></div>
+    <div className="tw-absolute tw-inset-0">
+      <div className="tw-relative tw-z-20 tw-flex tw-h-full tw-flex-col tw-justify-center tw-items-center tw-text-center">
+        <IonCard className="tw-bg-background">
+          <IonCardContent className="tw-text-text">
+            <h1 className="!tw-mb-4">{t("loopIsNotPublished")}</h1>
+            {isUserHost ? null : (
+              <>
+                <p className="!tw-mb-3">{t("pleaseContactYourLoopHost")}</p>
+                <div className="tw-flex tw-justify-center tw-flex-wrap tw-mt-1.5 tw-m-0 tw-mb-2.5">
+                  {hosts.map((host) => (
+                    <IonButton
+                      key={host.uid}
+                      size="small"
+                      routerLink={"/address/" + host.uid}
+                      color="light"
+                      className="tw-m-1.5 tw-text-base"
+                    >
+                      {host.name}
+                    </IonButton>
+                  ))}
+                </div>
+              </>
+            )}
+            <IonButton
+              onClick={goToSettings}
+              expand="block"
+              color="primary"
+              fill={isUserHost ? "outline" : "solid"}
+            >
+              {t("selectADifferentLoop")}
+            </IonButton>
+            {isUserHost ? (
+              <>
+                <IonButton
+                  href={`https://clothingloop.org/loops/${chain?.uid}/members`}
+                  target="_blank"
+                  expand="block"
+                  className="tw-mt-4"
+                >
+                  {t("editLoop")}
+                  <IonIcon slot="end" icon={openOutline}></IonIcon>
+                </IonButton>
+                <IonButton
+                  expand="block"
+                  className="tw-mt-4"
+                  color="light"
+                  onClick={() => closeOverlayPaused()}
+                >
+                  {t("hide")}
+                </IonButton>
+              </>
+            ) : null}
+          </IonCardContent>
+        </IonCard>
       </div>
-    </IonPage>
+      <div className="tw-absolute tw-z-10 tw-inset-0 tw-bg-paused tw-opacity-20"></div>
+    </div>
   );
 }
