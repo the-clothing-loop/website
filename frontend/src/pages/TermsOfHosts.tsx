26a041122d0987bd2687668f75a393f046b1039f
Lucian I. Last <li@last.nl>
2023-12-14 22:16:33 +0100

    Update text from "[EXT] Terms of Hosting - GT comments 21 October 2023(12060942.1)"

--- frontend/src/pages/TermsOfHosts.tsx
+++ frontend/src/pages/TermsOfHosts.tsx
@@ -13,11 +13,328 @@ export default function TermsOfHosts() {
         </h1>
 
         <div className="prose">
-          <p>Version dated 2023, December 11th</p>
+          <p>Version dated 2023, December 21th</p>
 
           <h2>Terms of Hosts</h2>
 
-          <p>...</p>
+          <p>
+            Provided by Stichting Slow Fashion Movement (hereinafter also
+            referred to as &quot;we&quot; or &quot;us&quot;).
+          </p>
+          <h2>Applicability and amendments</h2>
+          <ol>
+            <li>
+              These terms of use for hosts, as amended from time to time
+              (hereinafter referred to as the &quot;Terms of Hosting&quot;),
+              stipulate the terms of your participation in The Clothing Loop as
+              a host.
+            </li>
+            <li>
+              By signing up to become a host of one or more local clothing loops
+              of The Clothing Loop (hereinafter also referred to as a ″Loop″),
+              you (hereinafter also referred to as a &quot;Host&quot;) confirm
+              that you are of legal age in your jurisdiction of residence to
+              enter into a binding agreement and that you agree to these Terms
+              of Hosting.
+            </li>
+            <li>
+              These Terms of Hosting are in addition to our Terms of Use, which
+              remain applicable to you. The Terms of Use can be found here:
+              [link]. In case of conflicting provisions between the Terms of
+              Hosting and the Terms of Use, the provisions of the Terms of
+              Hosting prevail.
+            </li>
+            <li>
+              We reserve the right to unilaterally amend these Terms of Hosting.
+              We will endeavour to inform you of any material changes to the
+              Terms of Hosting in a timely manner. In addition, we may at any
+              time modify the Platform (being our website, www.clothingloop.org,
+              our app, My Clothing Loop, and any other application(s) used by
+              us), change and/or delete data or restrict or discontinue the use
+              of the Platform or your participation in The Clothing Loop either
+              as a User (being a user of the Platform)/participant or a Host,
+              without any liability on any grounds whatsoever towards you or
+              other third parties and without you being able to derive any
+              rights therefrom.
+            </li>
+          </ol>
+          <h2>General rules of conduct</h2>
+          <ol start={5}>
+            <li>
+              The Clothing Loop is an easy-to-access initiative as part of
+              Stichting Slow Fashion Movement for swapping clothes with other
+              people in your area or neighbourhood. The goal of The Clothing
+              Loop is to make it easy and fun to share pre-loved clothes by
+              connecting people in local communities that share a bag of clothes
+              - free of charge. The Platform is solely intended to give Users
+              the opportunity to participate in The Clothing Loop. After they
+              have successfully registered, they will be connected to a Loop
+              near their residence to actively start participating. Although
+              participants and Hosts of a Loop are responsible for the
+              implementation and monitoring of the concept of The Clothing Loop,
+              and for all matters relating to the participation in a Loop and
+              the conduct within a Loop, you as a Host have a key role in making
+              sure the Loop runs smoothly – either independently, or together
+              with one or more additional Hosts. We are happy to assist with
+              tips and tricks, but we as Stichting Slow Fashion Movement have no
+              responsibility and liability in respect hereto. We are only here
+              to facilitate by providing the Platform, the rest is up to you and
+              the participants!
+            </li>
+            <li>
+              Your tasks and obligations as a Host include, among others, the
+              following:
+              <ul>
+                <li>
+                  organizing the relevant Loop. This includes for example:
+                  <ol>
+                    <li>
+                      sending each new participant a message as soon as possible
+                      after the participant has registered, but in any case,
+                      within 4 weeks after registration to inform the
+                      participant about how things are arranged within your
+                      specific Loop;
+                    </li>
+                    <li>
+                      introducing the new participant to the other participants
+                      of the Loop;
+                    </li>
+                    <li>
+                      placing the new participant in a logical place in the
+                      route of the Loop; and
+                    </li>
+                    <li>
+                      being the point of contact for participants of the Loop in
+                      case of questions, complaints or suggestions;
+                    </li>
+                  </ol>
+                </li>
+                <li>
+                  procure that participants abide by the general rules of
+                  conduct applicable to them as described in the Terms of Use
+                  and any rules agreed to by the participants of the Loop;
+                </li>
+                <li>
+                  issuing a warning to participants if they do not abide by the
+                  rules as referred to above;
+                </li>
+                <li>
+                  remove participants from the Loop if they no longer wish to
+                  participate or if they do not abide by the rules as referred
+                  to above;
+                </li>
+                <li>
+                  introducing and managing the bags that circulate within the
+                  Loop; and
+                </li>
+                <li>
+                  regularly checking if the quality of the clothes is good
+                  enough to exchange them.
+                </li>
+              </ul>
+            </li>
+            <li>
+              As Host, you will also be the point of contact between the Loop
+              and us.
+            </li>
+            <li>
+              It is not allowed to use the Platform or the Clothing Loop
+              (including a specific Loop) for purposes other than the purpose
+              stipulated above, such as (but not only) commercial purposes,
+              unless we have provided our prior written consent to do so. It is
+              also not permitted to offer goods, funds, or services in exchange
+              for any person’s participation in a Loop, nor is it permitted to
+              charge others (or demand or request any consideration) for their
+              participation in or for any items that are exchanged as part of a
+              Loop.
+            </li>
+            <li>
+              You are responsible for and are required to use the Platform
+              carefully and to conduct yourself in a way that aligns with the
+              purpose and values of The Clothing Loop and Stichting Slow Fashion
+              Movement. You are also required to follow any instructions and
+              regulations issued by us as applicable. The foregoing includes
+              (among other things) that you:
+              <ul>
+                <li>must provide complete and truthful information;</li>
+                <li>may not share misleading or offensive information;</li>
+                <li>
+                  may not violate the rights of ownership and/or other
+                  (personal) rights of other Users/participants or third parties
+                  (including intellectual property rights);
+                </li>
+                <li>
+                  may not perform or permit any acts and/or conduct that
+                  violates applicable laws or regulations, public morals, public
+                  order or the rights of other Users/participants or third
+                  parties (including the rights of Stichting Slow Fashion
+                  Movement);
+                </li>
+                <li>
+                  must conduct yourself in a respectful and cordial manner in
+                  all communications and interactions with other
+                  Users/participants and you must adhere to the rules and
+                  regulations applicable to any (communication) channels or
+                  applications used by the Loop;
+                </li>
+                <li>
+                  may not add to the Platform or place via any other
+                  (communication) channels or applications used by the Loop any
+                  texts, data or files that have illegal or offensive content,
+                  for example, discriminatory, racist, sexist or pornographic
+                  content, or (otherwise) infringe on the lives of other
+                  Users/participants or third parties;
+                </li>
+                <li>must keep your own login data confidential;</li>
+                <li>
+                  must keep the personal data of other Users/participants or
+                  third parties that may become available to you strictly
+                  confidential and, if requested do to so, immediately destroy
+                  such personal data;
+                </li>
+                <li>
+                  insofar you share (personal) data of other Users/participants
+                  or third parties with us (for example, names, telephone
+                  numbers and email addresses), you guarantee that you are
+                  authorized to share this data with us in accordance with
+                  applicable laws and regulations (for example, because you have
+                  permission to do so) and that we can use this data for the
+                  purposes for which this data has been shared with us;
+                </li>
+                <li>
+                  may not spread viruses through the Platform or through any
+                  (communication) channels or applications used by the Loop, or
+                  otherwise disrupt, prevent or damage (the operation of) the
+                  Platform or any (communication) channels or applications used
+                  by the Loop; and
+                </li>
+                <li>
+                  must respect and consider the good name and reputation of The
+                  Clothing Loop and Stichting Slow Fashion Movement.
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              In the event of (suspected) conduct in violation of these Terms of
+              Hosting or contrary to the purpose and values of The Clothing Loop
+              and Stichting Slow Fashion Movement, we may take any action we
+              deem necessary and appropriate, including (but not limited to)
+              denying access to the Platform, removing you from specific Loops,
+              or prohibit you from participating in The Clothing Loop, in whole
+              or in part, temporarily or permanently, and recover any damages
+              and/or costs from you. We may also inform the competent
+              institutions and authorities.
+            </li>
+          </ol>
+          <h2>
+            Use of other media and/or communication channels and applications
+          </h2>
+          <ol>
+            <li>
+              Each Loop decides which channels or applications to use to
+              communicate with participants within the Loop. Stichting Slow
+              Fashion Movement is not responsible or liable for the use of any
+              such channels or applications. The Host(s) and participants of a
+              Loop are and remain responsible for all matters relating to the
+              use of any chosen channel and/or application.
+            </li>
+          </ol>
+          <h2>Liability</h2>
+          <ol>
+            <li>
+              The use of the Platform, and thus participating in The Clothing
+              Loop (including any Loop) is at your own risk and, if applicable,
+              expense.
+            </li>
+            <li>
+              We cannot exclude and do not guarantee that the Platform does not
+              or cannot have any defects. Interruptions, errors, viruses,
+              malware or other defects may occur. In addition, the information
+              on the Platform may not be accurate, complete or up to date. The
+              Clothing Loop is not responsible of liable for these defects.
+            </li>
+            <li>
+              The Clothing Loop shall not be liable for any damage or injury,
+              physical or otherwise (including as a result of viruses and
+              bacteria) resulting from the use of the Platform or participating
+              in The Clothing Loop (including, for the avoidance of doubt, any
+              Loop), unless direct damages are the result of demonstrable intent
+              or gross negligence on our part.
+            </li>
+            <li>
+              The foregoing also means that we are under no circumstances liable
+              for indirect or consequential damages, such as loss of sales or
+              income. You indemnify us against all possible third-party claims
+              resulting from the use of the Platform or participation in The
+              Clothing Loop (including, for the avoidance of doubt, any Loop).
+            </li>
+          </ol>
+          <h2>Intellectual property rights</h2>
+          <ol>
+            <li>
+              Unless otherwise stated, all intellectual property rights and/or
+              similar rights in or to the Platform belong exclusively to us or
+              our licensors. This includes (among other things) the underlying
+              software, texts, images, videos and audio clips. You may make
+              copies of the information and images on the Platform exclusively
+              for your own use, for example by printing or storing them. The
+              commercial use of information and images of the Platform is
+              expressly prohibited.
+            </li>
+            <li>
+              The ownership (insofar that is the case) and responsibility of the
+              texts, data and files placed by you on the Platform or shared
+              withing a Loop remains yours. However, you agree that we can use
+              and distribute these texts, data and files without restriction for
+              any purpose, without acknowledgement and without owing you any
+              compensation, and insofar legally permitted. To the extent
+              necessary, you hereby grant us a free, royalty-free, perpetual,
+              irrevocable, worldwide, non-exclusive, and fully transferable and
+              sub-licensable right and license to use, reproduce, display,
+              distribute, adapt and modify all texts, data and files posted by
+              you, create derivative works from them and otherwise exploit them
+              in any non-commercial way. Please note that you should not share
+              texts, data and files placed or shared by other Hosts,
+              Users/participant with other parties, unless you have explicit and
+              legally obtained consent from the relevant party.{" "}
+            </li>
+          </ol>
+          <h2>Privacy and Cookie Policy</h2>
+          <ol>
+            <li>
+              Please be referred to our{" "}
+              <a
+                href="https://www.clothingloop.org/privacy-policy"
+                title="https://www.clothingloop.org/privacy-policy"
+              >
+                Privacy &amp; Cookie policy
+              </a>
+              , which also applies when using the Platform.
+            </li>
+          </ol>
+          <h2>Choice of law and other provisions</h2>
+          <ol>
+            <li>
+              If these Terms of Hosting are or become partially invalid, we and
+              you will continue to be bound by the remaining, applicable
+              provisions.
+            </li>
+            <li>
+              The English version of these Terms of Hosting is binding.
+              Translations are provided for convenience only.
+            </li>
+            <li>
+              These Terms of Hosting shall be governed, to the extent possible,
+              by Dutch law. The applicability of the Vienna Convention on
+              Contracts for the International Sale of Goods is excluded. In the
+              event of any dispute relating to these Terms of Hosting (including
+              the applicability of these Terms of Hosting), we will try to
+              resolve such dispute together. If that is not possible, the
+              competent court in Amsterdam will have exclusive jurisdiction to
+              settle the dispute, to the extent legally permissible.
+            </li>
+          </ol>
 
           <h2>Contact details</h2>
 
@@ -37,6 +354,9 @@ export default function TermsOfHosts() {
               <br />
               The Netherlands
             </address>
+          </p>
+          <p>
+            <i>(KVK 85110701)</i>
           </p>
         </div>
       </div>
