cee67b14f81cc4a2a559a7ecc0941d21336fe689
Lucian I. Last <li@last.nl>
2023-12-19 12:44:09 +0100

    Unnecessary err var name

--- server/internal/services/notification.go
+++ server/internal/services/notification.go
@@ -1,11 +1,12 @@
 package services
 
 import (
+	"strconv"
+
 	"github.com/the-clothing-loop/website/server/internal/app"
 	"github.com/the-clothing-loop/website/server/internal/models"
 	"github.com/the-clothing-loop/website/server/internal/views"
 	"gorm.io/gorm"
-	"strconv"
 )
 
 func SendElapsedNotifications(db *gorm.DB) error {
@@ -14,18 +15,18 @@ func SendElapsedNotifications(db *gorm.DB) error {
 		return err
 	}
 	for _, notification := range notifications {
-		sendErr := app.OneSignalCreateNotification(
+		err := app.OneSignalCreateNotification(
 			db,
 			[]string{strconv.FormatUint(uint64(notification.UserID), 10)},
 			*views.Notifications[notification.Title],
 			*views.Notifications[notification.Content],
 		)
-		if sendErr != nil {
-			return sendErr
+		if err != nil {
+			return err
 		}
-		deleteErr := notification.Delete(db)
-		if deleteErr != nil {
-			return deleteErr
+		err = notification.Delete(db)
+		if err != nil {
+			return err
 		}
 	}
 	return nil
