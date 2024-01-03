a591162fa8f088b3be8c51e7b807a0ac9a80d6cd
Lucian I. Last <li@last.nl>
2024-01-03 16:59:03 +0100

    Add mailpit to acceptance

--- devops/lint-caddy-files.sh
+++ devops/lint-caddy-files.sh
@@ -2,8 +2,8 @@
 
 pushd templates
 
-caddy validate --config ./Caddyfile && \
-caddy validate --config ./Caddyfile.maint
+caddy validate --config ./Caddyfile || exit 1
+caddy validate --config ./Caddyfile.maint || exit 1
 echo ./Caddyfile | caddy fmt --overwrite
 echo ./Caddyfile.maint | caddy fmt --overwrite
 
