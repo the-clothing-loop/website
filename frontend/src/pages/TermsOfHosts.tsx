dbdb2cd9602033680de0e409049731f65cbf3d86
Lucian I. Last <li@last.nl>
2024-01-04 12:45:11 +0100

    Title Terms of Hosts

--- frontend/src/pages/TermsOfHosts.tsx
+++ frontend/src/pages/TermsOfHosts.tsx
@@ -5,11 +5,11 @@ export default function TermsOfHosts() {
     <>
       <Helmet>
         <title>The Clothing Loop | Terms of Hosts</title>
-        <meta name="description" content="Terms of use" />
+        <meta name="description" content="Terms of Hosts" />
       </Helmet>
       <div className="container mx-auto px-4 md:px-20 py-10">
         <h1 className="font-serif text-secondary font-bold text-5xl mb-4">
-          {"Terms of Use - The Clothing Loop"}
+          {"Terms of Hosts - The Clothing Loop"}
         </h1>
 
         <TermsOfHostsHTML className="prose" />
