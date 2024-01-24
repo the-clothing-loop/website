62426abeef75e9511a2c0b9ca495bd801fc01b49
Lucian I. Last <li@last.nl>
2024-01-24 13:32:48 +0100

    Add more sql scripts for the migration

--- server/sql/migration-latlong.sql
+++ server/sql/migration-latlong.sql
@@ -4,12 +4,18 @@
 ALTER TABLE users ADD COLUMN longitude_migration double AFTER longitude;
 ALTER TABLE users ADD COLUMN latitude_migration double AFTER latitude;
 
-UPDATE users SET longitude_migration = latitude, latitude_migration = longitude
-WHERE longitude > latitude;
+-- Reset latlong_migration columns if zero to null
+UPDATE users SET longitude_migration = NULL, latitude_migration = NULL
+WHERE longitude_migration = 0 AND latitude_migration = 0;
 
-UPDATE users SET longitude = longitude_migration, latitude = latitude_migration
+-- Reverse columns
+UPDATE users SET longitude_migration = latitude, latitude_migration = longitude;
+
+-- !Reset all latlong_migration columns!
+UPDATE users SET longitude_migration = NULL, latitude_migration = NULL
 WHERE longitude_migration IS NOT NULL OR latitude_migration IS NOT NULL;
 
+-- Remove latlong_migration columns
 ALTER TABLE users DROP COLUMN longitude_migration;
 ALTER TABLE users DROP COLUMN latitude_migration;
 
@@ -21,3 +27,6 @@ AND
 	longitude>48 AND longitude<55
 )
 ORDER BY updated_at DESC;
+
+-- Find latlong_migration columns todos
+SELECT id, name, address, latitude, latitude_migration, longitude, longitude_migration FROM users WHERE latitude_migration IS NOT NULL;
