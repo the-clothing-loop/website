7cf7da3225c0d434ca58d34db14b1ea6b48e3ced
Lucian I. Last <li@last.nl>
2024-02-05 14:36:09 +0100

    Fix Address list background element

--- app/src/pages/AddressList.tsx
+++ app/src/pages/AddressList.tsx
@@ -72,110 +72,112 @@ export default function AddressList() {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle
-              size="large"
-              className="tw-font-serif tw-text-red dark:tw-text-red-contrast"
-            >
-              {t("addresses")}
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonList className={shouldBlur ? "tw-blur" : ""}>
-          {route.map((userUID, i) => {
-            const user = chainUsers.find((u) => u.uid === userUID);
-            if (!user) return null;
-            const isMe = user.uid === authUser?.uid;
-            const isUserPaused = isPaused(user.paused_until);
-            const isPrivate = IsPrivate(user.email);
-            const isAddressPrivate = IsPrivate(user.address);
-            const userBags = bags.filter((b) => b.user_uid === user.uid);
-            const isUserHost =
-              user.chains.find((uc) => uc.chain_uid === chain?.uid)
-                ?.is_chain_admin || false;
-            return (
-              <IonItem
-                lines="full"
-                routerLink={isPrivate ? undefined : "/address/" + user.uid}
-                key={user.uid}
-                color={isUserPaused ? "light" : undefined}
+        <div className="tw-relative tw-min-h-full">
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle
+                size="large"
+                className="tw-font-serif tw-text-red dark:tw-text-red-contrast"
               >
-                <IonText className="ion-text-ellipsis tw-my-1.5">
-                  <h5
-                    className={`ion-no-margin ${
-                      isMe
-                        ? "tw-text-primary"
-                        : isUserPaused
-                        ? "tw-text-medium"
-                        : ""
-                    }`}
-                  >
-                    {user.name}
+                {t("addresses")}
+              </IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonList className={shouldBlur ? "tw-blur" : ""}>
+            {route.map((userUID, i) => {
+              const user = chainUsers.find((u) => u.uid === userUID);
+              if (!user) return null;
+              const isMe = user.uid === authUser?.uid;
+              const isUserPaused = isPaused(user.paused_until);
+              const isPrivate = IsPrivate(user.email);
+              const isAddressPrivate = IsPrivate(user.address);
+              const userBags = bags.filter((b) => b.user_uid === user.uid);
+              const isUserHost =
+                user.chains.find((uc) => uc.chain_uid === chain?.uid)
+                  ?.is_chain_admin || false;
+              return (
+                <IonItem
+                  lines="full"
+                  routerLink={isPrivate ? undefined : "/address/" + user.uid}
+                  key={user.uid}
+                  color={isUserPaused ? "light" : undefined}
+                >
+                  <IonText className="ion-text-ellipsis tw-my-1.5">
+                    <h5
+                      className={`ion-no-margin ${
+                        isMe
+                          ? "tw-text-primary"
+                          : isUserPaused
+                          ? "tw-text-medium"
+                          : ""
+                      }`}
+                    >
+                      {user.name}
 
-                    {isUserHost ? (
-                      <IonIcon
-                        icon={shield}
-                        color="medium"
-                        className="tw-w-4 tw-h-4 tw-m-0 tw-ml-[5px] tw-align-text-top"
-                      />
-                    ) : null}
-                  </h5>
-                  <span className="tw-opacity-60">
+                      {isUserHost ? (
+                        <IonIcon
+                          icon={shield}
+                          color="medium"
+                          className="tw-w-4 tw-h-4 tw-m-0 tw-ml-[5px] tw-align-text-top"
+                        />
+                      ) : null}
+                    </h5>
+                    <span className="tw-opacity-60">
+                      {isUserPaused ? (
+                        <small>{t("paused")}</small>
+                      ) : isAddressPrivate ? (
+                        <small>&nbsp;</small>
+                      ) : (
+                        <small>{user.address}</small>
+                      )}
+                    </span>
+                  </IonText>
+                  <IonText
+                    slot="start"
+                    color="medium"
+                    className="!tw-font-bold tw-w-[30px] tw-whitespace-nowrap"
+                  >
                     {isUserPaused ? (
-                      <small>{t("paused")}</small>
-                    ) : isAddressPrivate ? (
-                      <small>&nbsp;</small>
+                      <IonIcon
+                        icon={pauseCircleSharp}
+                        color="medium"
+                        className="tw-w-6 tw-h-6 tw-m-0 tw-align-text-top "
+                      />
                     ) : (
-                      <small>{user.address}</small>
+                      <span>{"#" + (i + 1)}</span>
                     )}
-                  </span>
-                </IonText>
-                <IonText
-                  slot="start"
-                  color="medium"
-                  className="!tw-font-bold tw-w-[30px] tw-whitespace-nowrap"
-                >
-                  {isUserPaused ? (
-                    <IonIcon
-                      icon={pauseCircleSharp}
-                      color="medium"
-                      className="tw-w-6 tw-h-6 tw-m-0 tw-align-text-top "
-                    />
-                  ) : (
-                    <span>{"#" + (i + 1)}</span>
-                  )}
-                </IonText>
-                <div
-                  slot="end"
-                  style={{
-                    width:
-                      userBags.length < 4
-                        ? 0
-                        : 20 * Math.floor(userBags.length / 2),
-                    paddingBottom: userBags.length < 4 ? 20 : 0,
-                  }}
-                  className="tw-flex tw-flex-col tw-h-10 tw-flex-wrap-reverse tw-items-end"
-                >
-                  {userBags.map((b) => (
-                    <IonIcon
-                      icon={bagHandle}
-                      style={{ color: b.color }}
-                      className="tw-m-0.5"
-                      key={b.id}
-                    />
-                  ))}
-                </div>
-              </IonItem>
-            );
-          })}
-        </IonList>
-        <IonIcon
-          aria-hidden="true"
-          icon="/the_clothing_loop_logo_cropped.svg"
-          style={{ fontSize: 150 }}
-          className="tw-absolute tw-w-full tw-min-h-auto tw-invert-[60%] tw-bottom-0 tw-mb-8 tw-overflow-hidden tw-stroke-text dark:tw-stroke-light-tint"
-        />
+                  </IonText>
+                  <div
+                    slot="end"
+                    style={{
+                      width:
+                        userBags.length < 4
+                          ? 0
+                          : 20 * Math.floor(userBags.length / 2),
+                      paddingBottom: userBags.length < 4 ? 20 : 0,
+                    }}
+                    className="tw-flex tw-flex-col tw-h-10 tw-flex-wrap-reverse tw-items-end"
+                  >
+                    {userBags.map((b) => (
+                      <IonIcon
+                        icon={bagHandle}
+                        style={{ color: b.color }}
+                        className="tw-m-0.5"
+                        key={b.id}
+                      />
+                    ))}
+                  </div>
+                </IonItem>
+              );
+            })}
+          </IonList>
+          <IonIcon
+            aria-hidden="true"
+            icon="/the_clothing_loop_logo_cropped.svg"
+            style={{ fontSize: 150 }}
+            className="tw-w-full -tw-mb-2 tw-invert-[60%] tw-overflow-hidden tw-stroke-text dark:tw-stroke-light-tint"
+          />
+        </div>
       </IonContent>
     </IonPage>
   );
