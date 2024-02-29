54ab4f317ac501d4d788f31f1197eec5f7da06ca
Lucian I. Last <li@last.nl>
2024-02-29 11:33:04 +0100

    Change order authentication

--- server/internal/app/auth/token.go
+++ server/internal/app/auth/token.go
@@ -22,19 +22,22 @@ type MyJwtClaims struct {
 }
 
 func TokenReadFromRequest(c *gin.Context) (string, bool) {
-	token, ok := cookieRead(c)
-	if !ok {
-		prefix := "Bearer "
-		// fallback to bearer token for testing
-		a := c.Request.Header.Get("Authorization")
-		_, t, ok := strings.Cut(a, prefix)
-		if !ok {
-			return "", false
-		}
-		token = t
+	// read cookie first
+	prefix := "Bearer "
+	// fallback to bearer token for testing
+	a := c.Request.Header.Get("Authorization")
+	_, token, ok := strings.Cut(a, prefix)
+	if ok {
+		return token, true
 	}
 
-	return token, true
+	// if no cookie set then read authorization header
+	token, ok = cookieRead(c)
+	if ok {
+		return token, true
+	}
+
+	return "", false
 }
 
 func OtpCreate(db *gorm.DB, userID uint) (string, error) {
