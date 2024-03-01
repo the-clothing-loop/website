3b409340c5c3cd66d80786964f2e2065f6fc80b8
Lucian I. Last <li@last.nl>
2024-03-02 00:43:07 +0100

    add vitest

---
+++ frontend/src/components/react/util/is_ssr.test.ts
@@ -0,0 +1,11 @@
+import { expect, test, vi } from "vitest";
+import isSSR from "./is_ssr";
+
+test("server side rendering check", () => {
+  //@ts-ignore
+  globalThis["window"] = undefined;
+  expect(isSSR(), "without globalThis.window you are running in node");
+  //@ts-ignore
+  globalThis["window"] = true;
+  expect(!isSSR(), "with globalThis.window you are running in browser");
+});
