1f4f79b693569bf46345af67405fec8f44fb2057
Lucian I. Last <li@last.nl>
2024-03-04 08:45:36 +0100

    Fix connection with docker

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
     server: {
       proxy: {
         "/api": {
-          target: "http://localhost:8084",
+          target: "http://server:8084",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
