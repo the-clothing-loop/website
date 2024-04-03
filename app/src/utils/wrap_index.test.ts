71690c10fbfd75906ff3c466a3577036b82dd4f1
Lucian I. Last <li@last.nl>
2024-04-03 16:54:06 +0200

    Fix test

--- app/src/utils/wrap_index.test.ts
+++ app/src/utils/wrap_index.test.ts
@@ -4,8 +4,12 @@ import wrapIndex from "./wrap_index";
 test("should wrap index correctly", () => {
   // prettier-ignore
   const tests = [
-      {l: 3, i: 1, ex: 1},
-      {l: [1,2,3], i: 1, ex: 1},
+    {l: 3, i: 3, ex: 0},
+    {l: 3, i: 0, ex: 0},
+    {l: 3, i: 6, ex: 0},
+    {l: 3, i: 1, ex: 1},
+    {l: 3, i: 4, ex: 1},
+    {l: 3, i: -1, ex: 2},
    ]
   for (const { l, i, ex } of tests) {
     expect(wrapIndex(i, l)).toBe(ex);
