3e8ed44cf91aff3fcd2396c7a528ea90056e145b
Natalie <nataliewilson519@gmail.com>
2024-04-12 11:35:49 -0700

    Fix send message input moving

--- app/src/pages/Chat.tsx
+++ app/src/pages/Chat.tsx
@@ -206,50 +206,49 @@ export default function Chat() {
       </IonHeader>
       <IonContent
         fullscreen
-        class={isThemeDefault ? " tw-bg-purple-contrast" : ""}
+        class={isThemeDefault ? "tw-bg-purple-contrast" : ""}
       >
-        <div>
-          <div className="tw-fixed tw-bg-[#f7f4fb] tw-z-10 tw-w-full">
-            <div className="tw-flex tw-ml-5 tw-mb-2 tw-mt-2 ">
-              {chatRooms?.map((cr) => {
-                let initials = "";
-                cr.name.split(" ").forEach((word) => {
-                  initials += word[0];
-                });
-                return (
-                  <div className="tw-rounded-full tw-w-14 tw-h-14 tw-mr-4 tw-bg-purple-shade tw-flex tw-items-center tw-justify-center">
-                    <div className="tw-font-bold">{initials}</div>
-                  </div>
-                );
-              })}
-            </div>
+        <div className="tw-fixed tw-bg-light tw-z-10 tw-w-full">
+          <div className="tw-flex tw-ml-5 tw-mb-2 tw-mt-2 ">
+            {chatRooms?.map((cr) => {
+              let initials = "";
+              cr.name.split(" ").forEach((word) => {
+                initials += word[0];
+              });
+              return (
+                <div className="tw-rounded-full tw-w-14 tw-h-14 tw-mr-4 tw-bg-purple-shade tw-flex tw-items-center tw-justify-center">
+                  <div className="tw-font-bold">{initials}</div>
+                </div>
+              );
+            })}
           </div>
-          <div className="">
-            <div className="tw-flex tw-flex-col-reverse">
-              {postList.order.map((item) => {
-                let post = postList.posts[item];
-                return (
-                  <div>
-                    <IonItem
-                      color="light"
-                      key={post.id}
-                      className={`tw-rounded-tl-2xl tw-rounded-tr-2xl tw-mb-2 ${
-                        post.user_id == authUser?.uid
-                          ? "tw-rounded-bl-2xl tw-float-right tw-ml-8 tw-mr-4"
-                          : "tw-rounded-br-2xl tw-mr-8 tw-ml-4 tw-float-left"
-                      }`}
-                    >
-                      <div className="tw-py-2">
-                        <div className="tw-font-bold">
-                          {post.props.username}
-                        </div>
-                        <div>{post.message}</div>
-                      </div>
-                    </IonItem>
-                  </div>
-                );
-              })}
-            </div>
+        </div>
+
+        <div className="tw-flex tw-flex-col tw-h-full">
+          <div className="tw-flex-grow tw-overflow-y-auto tw-flex tw-flex-col-reverse">
+            {postList.order.map((item) => {
+              let post = postList.posts[item];
+              return (
+                <div>
+                  <IonItem
+                    color="light"
+                    key={post.id}
+                    className={`tw-rounded-tl-2xl tw-rounded-tr-2xl tw-mb-2 ${
+                      post.user_id == authUser?.uid
+                        ? "tw-rounded-bl-2xl tw-float-right tw-ml-8 tw-mr-4"
+                        : "tw-rounded-br-2xl tw-mr-8 tw-ml-4 tw-float-left"
+                    }`}
+                  >
+                    <div className="tw-py-2">
+                      <div className="tw-font-bold">{post.props.username}</div>
+                      <div>{post.message}</div>
+                    </div>
+                  </IonItem>
+                </div>
+              );
+            })}
+          </div>
+          <div>
             <IonItem
               color="light"
               disabled={sendingMsg == SendingMsgState.SENDING}
