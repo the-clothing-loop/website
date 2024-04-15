10b14fca1f49a27cdb1b47d17c2341a7f9d3fcce
Lucian I. Last <li@last.nl>
2024-04-15 15:42:59 +0200

    improve errors in slog

--- server/pkg/httperror/httperror.go
+++ server/pkg/httperror/httperror.go
@@ -28,12 +28,13 @@ func New(status int, errOrMessage any) *HttpError {
 
 func (e *HttpError) StatusWithError(c *gin.Context) {
 	c.Error(e.Error)
+	errStr := e.Error.Error()
 	if 100 <= e.Status && e.Status < 300 {
-		slog.Info(e.Error.Error())
+		slog.Info("http error", "status", e.Status, "err", e.Error)
 	} else if 300 <= e.Status && e.Status < 400 {
-		slog.Warn(e.Error.Error())
+		slog.Warn("http error", "status", e.Status, "err", e.Error)
 	} else {
-		slog.Error(e.Error.Error())
+		slog.Error("http error", "status", e.Status, "err", e.Error)
 	}
-	c.String(e.Status, e.Error.Error())
+	c.String(e.Status, errStr)
 }
