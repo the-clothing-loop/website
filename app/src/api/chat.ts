931ba17a5015d2c26035d9f044a2d839568b1fdc
Lucian I. Last <li@last.nl>
2024-04-15 15:45:36 +0200

    lint

--- app/src/api/chat.ts
+++ app/src/api/chat.ts
@@ -1,16 +1,16 @@
 import axios from "./axios";
 export interface RespPatchGetOrJoinRoom {
-   chat_team: string;
-   chat_channel: string;
-   chat_user: string;
-   chat_token?: string;
- }
- export function patchGetOrJoinRoom(chainUID: string, renewToken: boolean) {
-   return axios.patch<RespPatchGetOrJoinRoom>(
-     `/v2/chat/${chainUID}/room`,
-     undefined,
-     {
-       params: { renew_token: renewToken },
-     },
-   );
- }
+  chat_team: string;
+  chat_channel: string;
+  chat_user: string;
+  chat_token?: string;
+}
+export function patchGetOrJoinRoom(chainUID: string, renewToken: boolean) {
+  return axios.patch<RespPatchGetOrJoinRoom>(
+    `/v2/chat/${chainUID}/room`,
+    undefined,
+    {
+      params: { renew_token: renewToken },
+    },
+  );
+}
