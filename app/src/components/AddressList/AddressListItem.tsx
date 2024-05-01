de9edf8e7ea261ae76011b43c57c83ec70cde22a
Lucian I. Last <li@last.nl>
2024-05-01 10:15:50 +0200

    Complete isMe addressListItem

--- app/src/components/AddressList/AddressListItem.tsx
+++ app/src/components/AddressList/AddressListItem.tsx
@@ -1,6 +1,6 @@
 import { IonItem, IonText, IonIcon } from "@ionic/react";
 import { t } from "i18next";
-import { shield, pauseCircleSharp } from "ionicons/icons";
+import { shield, pauseCircleSharp, bag } from "ionicons/icons";
 import { Bag, User } from "../../api/types";
 import BagSVG from "../Bags/Svg";
 
@@ -30,64 +30,81 @@ export default function AddressListItem({
     <IonItem
       lines="full"
       routerLink={routerLink}
-      color={isUserPaused ? "light" : undefined}
+      color={isMe ? "primary" : isUserPaused ? "light" : undefined}
+      id={"ali-" + user.uid}
     >
-      <IonText className="ion-text-ellipsis tw-my-1.5">
-        <h5
-          className={`ion-no-margin ${
-            isMe ? "tw-text-primary" : isUserPaused ? "tw-text-medium" : ""
-          }`}
-        >
-          {user.name}
-
-          {isHost ? (
-            <IonIcon
-              icon={shield}
-              color="medium"
-              className="tw-w-4 tw-h-4 tw-m-0 tw-ml-[5px] tw-align-text-top"
-            />
-          ) : null}
-        </h5>
-        <span className="tw-opacity-60">
-          {isUserPaused ? (
-            isChainAdmin ? (
-              <small>{user.address}</small>
-            ) : null
-          ) : isAddressPrivate ? (
-            <small>&nbsp;</small>
-          ) : (
-            <small>{user.address}</small>
-          )}
-        </span>
-      </IonText>
       <IonText
         slot="start"
-        color="medium"
-        className="!tw-font-bold tw-w-[30px] tw-whitespace-nowrap"
+        color={isMe ? "light" : "medium"}
+        className="!tw-font-bold tw-w-10 tw-mr-2.5 -tw-ml-2 tw-text-right tw-whitespace-nowrap"
       >
+        {isHost ? (
+          <IonIcon
+            icon={shield}
+            color={isMe ? undefined : "medium"}
+            className="tw-absolute tw-top-1 tw-left-1 tw-w-4 tw-h-4 tw-m-0"
+          />
+        ) : null}
         {isUserPaused ? (
           <IonIcon
             icon={pauseCircleSharp}
-            color="medium"
-            className="tw-w-6 tw-h-6 tw-m-0 tw-align-text-top "
+            color={isMe ? "light" : "medium"}
+            className="tw-w-6 tw-h-6 tw-m-0 tw-align-text-top"
           />
         ) : (
-          <span>{"#" + number}</span>
+          <span>
+            <small>#</small>
+            {number}
+          </span>
         )}
       </IonText>
-      <div
-        slot="end"
-        style={{
-          width: bags.length < 4 ? 0 : 20 * Math.floor(bags.length / 2),
-          paddingBottom: bags.length < 4 ? 20 : 0,
-        }}
-        className="tw-flex tw-flex-col tw-h-10 tw-flex-wrap-reverse tw-items-end"
-      >
-        {bags.map((b) => (
-          <div className="tw-w-4 tw-h-4" key={b.id}>
-            <BagSVG bag={{ number: "", color: b.color }} isList />
+      <div className="tw-flex tw-flex-row tw-w-full">
+        <div className="tw-flex-grow tw-flex tw-flex-col tw-justify-center ion-text-ellipsis tw-my-1.5">
+          <h5
+            className={`ion-no-margin ${
+              isMe ? "tw-text-light" : isUserPaused ? "tw-text-medium" : ""
+            }`}
+          >
+            {user.name}
+          </h5>
+          <span
+            className={"tw-inline-block tw-mt-0.5 ion-text-ellipsis tw-max-w-[220px] tw-text-xs ".concat(
+              isMe ? "tw-text-light" : "tw-opacity-60",
+            )}
+          >
+            {isUserPaused ? (
+              isChainAdmin ? (
+                <span>{user.address}</span>
+              ) : null
+            ) : isAddressPrivate ? (
+              <span>&nbsp;</span>
+            ) : (
+              <span>{user.address}</span>
+            )}
+          </span>
+        </div>
+        <div className="tw-flex tw-justify-end tw-items-center">
+          <div
+            dir="rtl"
+            className={"tw-grid tw-grid-rows-2 tw-gap-1 tw-mx-0.5"}
+            style={{
+              gridTemplateColumns: `repeat(${Math.ceil(
+                bags.length / 2,
+              )}, minmax(0, 1fr))`,
+            }}
+          >
+            {bags.map((b) => (
+              <div
+                className={"tw-w-4 tw-h-4 tw-mb-0.5".concat(
+                  isMe ? " tw-shadow-bags" : "",
+                )}
+                key={b.id}
+              >
+                <BagSVG bag={{ number: "", color: b.color }} isList />
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </IonItem>
   );
