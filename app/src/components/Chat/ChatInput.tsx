b531e75e6e4600cb65726c2c8d1b6557325bd4f2
Natalie <nataliewilson519@gmail.com>
2024-05-24 15:35:47 -0700

    add floating action button for bulky

--- app/src/components/Chat/ChatInput.tsx
+++ app/src/components/Chat/ChatInput.tsx
@@ -1,7 +1,28 @@
-import { IonButton, IonIcon, IonInput, IonItem } from "@ionic/react";
-import { sendOutline } from "ionicons/icons";
-import { useState } from "react";
+import {
+  IonButton,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonFabList,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonSelect,
+  IonSelectOption,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import {
+  chevronDownCircle,
+  colorPalette,
+  globe,
+  sendOutline,
+  shirt,
+} from "ionicons/icons";
+import { useRef, useState } from "react";
 import { Sleep } from "../../utils/sleep";
+import { shirtOutline, add, addOutline } from "ionicons/icons";
 
 export enum SendingMsgState {
   DEFAULT,
@@ -16,6 +37,8 @@ interface Props {
 export default function ChatInput(props: Props) {
   const [message, setMessage] = useState("");
   const [status, setStatus] = useState(SendingMsgState.DEFAULT);
+  const modal = useRef<HTMLIonModalElement>(null);
+  const addBulkyItem = useRef<HTMLIonSelectElement>(null);
 
   async function sendMessage() {
     setStatus(SendingMsgState.SENDING);
@@ -35,32 +58,52 @@ export default function ChatInput(props: Props) {
     e.preventDefault();
     sendMessage();
   }
-
+  function handleClickPlus() {
+    //setUpdateBulky(null);
+    addBulkyItem.current?.open();
+  }
   return (
-    <form className="tw-flex-shrink-0" onSubmit={onSubmit}>
-      <IonItem
-        lines="none"
-        color="light"
-        disabled={status == SendingMsgState.SENDING}
-      >
-        <IonInput
-          placeholder="Send Message"
-          value={message}
-          disabled={status == SendingMsgState.SENDING}
-          onIonInput={(e) => setMessage(e.detail.value as string)}
-          className="tw-ml-2"
-        />
-        <IonButton
-          slot="end"
-          shape="round"
-          disabled={status !== SendingMsgState.DEFAULT}
-          color="light"
-          className="tw-mr-0"
-          type="submit"
-        >
-          <IonIcon icon={sendOutline} color="primary" className="tw-text-2xl" />
-        </IonButton>
-      </IonItem>
-    </form>
+    <>
+    <div className="tw-bg-light">
+          <IonFab slot="fixed" vertical="bottom" horizontal="start" color="light">
+            <IonFabButton className="tw-h-7 tw-w-7 ">
+              <IonIcon icon={addOutline}></IonIcon>
+            </IonFabButton>
+            <IonFabList side="top" className="tw-my-10 tw-h-7 tw-w-7 ">
+              <IonFabButton size="small" className="tw-h-7 tw-w-7">
+                <IonIcon icon={shirtOutline}></IonIcon>
+              </IonFabButton>
+            </IonFabList>
+          </IonFab>
+        <form className="tw-flex-shrink-0 tw-ml-6" onSubmit={onSubmit}>
+          <IonItem
+            lines="none"
+            color="light"
+            disabled={status == SendingMsgState.SENDING}
+          >
+            <IonInput
+              placeholder="Send Message"
+              value={message}
+              disabled={status == SendingMsgState.SENDING}
+              onIonInput={(e) => setMessage(e.detail.value as string)}
+              className="tw-ml-2"
+            />
+            <IonButton
+              slot="end"
+              shape="round"
+              disabled={status !== SendingMsgState.DEFAULT}
+              color="light"
+              className="tw-mr-0"
+              type="submit"
+            >
+              <IonIcon
+                icon={sendOutline}
+                color="primary"
+                className="tw-text-2xl"
+              />
+            </IonButton>
+          </IonItem>
+        </form></div>
+    </>
   );
 }
