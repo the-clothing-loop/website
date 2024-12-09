7202d13741c5f2fa3a8c9d5f8a85f93d083e3140
Lucian I. Last <li@last.nl>
2024-12-09 14:34:52 +0100

    Working i18n

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,15 +1,45 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
+import getLanguages from "./src/languages";
+
+import reactI18next from "astro-react-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   site: import.meta.env.PUBLIC_BASE_URL,
-  integrations: [react(), astroI18next(), tailwind()],
+  integrations: [
+    reactI18next({
+      defaultLocale: "en",
+      locales: getLanguages(false),
+      defaultNamespace: "translation",
+      namespaces: [
+        "about",
+        "faq",
+        "testimonials",
+        "translation",
+        "contribute",
+        "survey",
+      ],
+      prefixDefaultLocale: true,
+      // localesDir: "/locales/",
+    }),
+    react(),
+    tailwind(),
+  ],
   server: { port: 3000 },
   outDir: "build",
+  // i18n: {
+  //   defaultLocale: "en",
+  //   locales: getLanguages(false),
+  //   routing: {
+  //     prefixDefaultLocale: true,
+  //   },
+  // },
+  redirects: {
+    "/": "/en/",
+  },
   vite: {
     server: {
       proxy: {
