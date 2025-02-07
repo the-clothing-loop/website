67214f2c5de9af3c5ad56049a20aba5d725f2460
Lucian I. Last <li@last.nl>
2025-02-07 14:19:04 +0100

    Use AssertNotErrorNow

--- server/internal/app/testutils.go
+++ server/internal/app/testutils.go
@@ -1,10 +1,12 @@
 package app
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/jaswdr/faker"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 )
 
@@ -19,4 +21,10 @@ func RunTestMain(m *testing.M, dbP **gorm.DB, configPath string) {
 
 	code := m.Run()
 	os.Exit(code)
+}
+
+func AssertNotErrorNow(t *testing.T, err error, msgAndArgs ...any) {
+	if err != nil {
+		assert.FailNow(t, fmt.Sprintf("Expected nil, but got: %#v", err), msgAndArgs...)
+	}
 }
